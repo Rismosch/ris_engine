17031c394510f6105f1fdfcff9fafd2dfc09349c
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-01-11 07:52:12 +0100

    added rapidjson test

--- code/program/program.cpp
+++ code/program/program.cpp
@@ -1,4 +1,8 @@
-#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <string>
+
+#include "../3rd_party/rapidjson/prettywriter.h"
 
 #include "flags.h"
 #include "../modules/risData/crc32.h"
@@ -7,6 +11,8 @@
 #include "../modules/risUtility/risFlag.h"
 #include "../modules/risUtility/risAllocator.h"
 #include "../modules/risUtility/risRandom.h"
+
+using namespace rapidjson;
 
 using namespace ris;
 using namespace risUtility;
@@ -162,23 +168,171 @@ void test_arguments(int argc, char* argv[])
 	}
 }
 
-class Person
-{
+#pragma region json_classes
+
+class Person {
 public:
-	Person(std::string name, U32 age, F32 favoriteNumber, bool isAwesome) : _name(std::move(name)), _age(age), _favorite_number(favoriteNumber), _is_awesome(isAwesome) {}
-	Person(const Person& rhs)=default;
-	~Person() = default;
+	Person(const std::string& name, unsigned age) : name_(name), age_(age) {}
+	Person(const Person& rhs) : name_(rhs.name_), age_(rhs.age_) {}
+	virtual ~Person();
+
+	Person& operator=(const Person& rhs) {
+		name_ = rhs.name_;
+		age_ = rhs.age_;
+		return *this;
+	}
+
 protected:
-	e
+	template <typename Writer>
+	void Serialize(Writer& writer) const {
+		// This base class just write out name-value pairs, without wrapping within an object.
+		writer.String("name");
+#if RAPIDJSON_HAS_STDSTRING
+		writer.String(name_);
+#else
+		writer.String(name_.c_str(), static_cast<SizeType>(name_.length())); // Supplying length of string is faster.
+#endif
+		writer.String("age");
+		writer.Uint(age_);
+	}
+
 private:
-	std::string _name;
-	U32 _age;
-	F32 _favorite_number;
-	bool _is_awesome;
+	std::string name_;
+	unsigned age_;
 };
+
+Person::~Person() {
+}
+
+class Education {
+public:
+	Education(const std::string& school, double GPA) : school_(school), GPA_(GPA) {}
+	Education(const Education& rhs) : school_(rhs.school_), GPA_(rhs.GPA_) {}
+
+	template <typename Writer>
+	void Serialize(Writer& writer) const {
+		writer.StartObject();
+
+		writer.String("school");
+#if RAPIDJSON_HAS_STDSTRING
+		writer.String(school_);
+#else
+		writer.String(school_.c_str(), static_cast<SizeType>(school_.length()));
+#endif
+
+		writer.String("GPA");
+		writer.Double(GPA_);
+
+		writer.EndObject();
+	}
+
+private:
+	std::string school_;
+	double GPA_;
+};
+
+class Dependent : public Person {
+public:
+	Dependent(const std::string& name, unsigned age, Education* education = 0) : Person(name, age), education_(education) {}
+	Dependent(const Dependent& rhs) : Person(rhs), education_(0) { education_ = (rhs.education_ == 0) ? 0 : new Education(*rhs.education_); }
+	virtual ~Dependent();
+
+	Dependent& operator=(const Dependent& rhs) {
+		if (this == &rhs)
+			return *this;
+		delete education_;
+		education_ = (rhs.education_ == 0) ? 0 : new Education(*rhs.education_);
+		return *this;
+	}
+
+	template <typename Writer>
+	void Serialize(Writer& writer) const {
+		writer.StartObject();
+
+		Person::Serialize(writer);
+
+		writer.String("education");
+		if (education_)
+			education_->Serialize(writer);
+		else
+			writer.Null();
+
+		writer.EndObject();
+	}
+
+private:
+
+	Education* education_;
+};
+
+Dependent::~Dependent() {
+	delete education_;
+}
+
+class Employee : public Person {
+public:
+	Employee(const std::string& name, unsigned age, bool married) : Person(name, age), dependents_(), married_(married) {}
+	Employee(const Employee& rhs) : Person(rhs), dependents_(rhs.dependents_), married_(rhs.married_) {}
+	virtual ~Employee();
+
+	Employee& operator=(const Employee& rhs) {
+		static_cast<Person&>(*this) = rhs;
+		dependents_ = rhs.dependents_;
+		married_ = rhs.married_;
+		return *this;
+	}
+
+	void AddDependent(const Dependent& dependent) {
+		dependents_.push_back(dependent);
+	}
+
+	template <typename Writer>
+	void Serialize(Writer& writer) const {
+		writer.StartObject();
+
+		Person::Serialize(writer);
+
+		writer.String("married");
+		writer.Bool(married_);
+
+		writer.String(("dependents"));
+		writer.StartArray();
+		for (std::vector<Dependent>::const_iterator dependentItr = dependents_.begin(); dependentItr != dependents_.end(); ++dependentItr)
+			dependentItr->Serialize(writer);
+		writer.EndArray();
+
+		writer.EndObject();
+	}
+
+private:
+	std::vector<Dependent> dependents_;
+	bool married_;
+};
+
+Employee::~Employee() {
+}
+
+#pragma endregion
 
 void test_json()
 {
 	std::cout << "\njson:" << std::endl;
 
+	std::vector<Employee> employees;
+
+	employees.push_back(Employee("Milo YIP", 34, true));
+	employees.back().AddDependent(Dependent("Lua YIP", 3, new Education("Happy Kindergarten", 3.5)));
+	employees.back().AddDependent(Dependent("Mio YIP", 1));
+
+	employees.push_back(Employee("Percy TSE", 30, false));
+
+	StringBuffer sb;
+	PrettyWriter<StringBuffer> writer(sb);
+
+	writer.StartArray();
+	for (std::vector<Employee>::const_iterator employeeItr = employees.begin(); employeeItr != employees.end(); ++employeeItr)
+		employeeItr->Serialize(writer);
+	writer.EndArray();
+
+	puts(sb.GetString());
 }
