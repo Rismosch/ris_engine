d9378b73d0ebe3a4d7cbd4a2044893be237d874a
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-02-01 08:38:26 +0100

    first work on compiler

--- risEngine/risModules/risResource/risResourceCompiler.cpp
+++ risEngine/risModules/risResource/risResourceCompiler.cpp
@@ -10,9 +10,18 @@ namespace risEngine
 
 	risCompilerError risResourceCompiler::compile()
 	{
-		const auto resource_redirect = "resource.redirect";
-		if (!file_exists(resource_redirect))
+		const auto resource_redirect_path = "resource.redirect";
+		if (!file_exists(resource_redirect_path))
 			return risCompilerError::REDIRECT_MISSING;
+
+		std::ifstream read_file;
+		read_file.open(resource_redirect_path);
+
+		read_file.seekg(0, std::ios_base::end);
+		auto length = read_file.tellg();
+		read_file.seekg(0, std::ios_base::beg);
+
+		read_file.close();
 
 		return risCompilerError::OK;
 	}
