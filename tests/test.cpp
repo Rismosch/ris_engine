17205e491927ac51002d5f985e43a39c54021da5
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-03-29 08:25:13 +0200

    added test project

---
+++ tests/test.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+
+TEST(TestCaseName, BruddaTest)
+{
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+	// https://docs.microsoft.com/de-de/visualstudio/test/how-to-use-google-test-for-cpp?view=vs-2022
+	// https://docs.microsoft.com/de-de/visualstudio/test/run-unit-tests-with-test-explorer?view=vs-2022
+	// https://github.com/google/googletest/blob/main/docs/primer.md
+}
