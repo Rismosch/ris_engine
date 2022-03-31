ad240f872d07ef7c658e5d03b0b0061a424cf743
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-03-31 20:55:34 +0200

    huh

--- tests/engine/test.cpp
+++ tests/engine/test.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 2);
-  EXPECT_TRUE(false);
+	EXPECT_EQ(1, 2);
+	EXPECT_TRUE(false);
+	// https://github.com/google/googletest/blob/main/docs/primer.md
 }
