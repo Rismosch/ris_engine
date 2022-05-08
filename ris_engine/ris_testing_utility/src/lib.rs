11de3ffbd3508c9a6627b42b4946b7f4f090d2ac
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-05-08 12:40:11 +0200

    refactored tests

--- ris_engine/ris_testing_utility/src/lib.rs
+++ ris_engine/ris_testing_utility/src/lib.rs
@@ -2,7 +2,7 @@ pub fn retry(retries: usize, test: fn() -> ()) {
     for _ in 0..retries - 1 {
         let result = std::panic::catch_unwind(test);
 
-        if let Ok(_) = result {
+        if result.is_ok() {
             return;
         }
     }
