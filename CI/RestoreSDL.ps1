c360194068b827492bbb90f1755f322231cc3cd2
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-05-22 08:14:18 +0200

    work on ps1 script

--- CI/RestoreSDL.ps1
+++ CI/RestoreSDL.ps1
@@ -1,6 +1,15 @@
-$sdl_include = [IO.Path]::Combine($PSScriptRoot, "..","SDL2","2.0.22","include")
-$headers = Get-ChildItem -Path $sdl_include
+$root = [IO.Path]::Combine($PSScriptRoot, "..")
+$ris_engine = [IO.Path]::Combine($root, "ris_engine")
+$sdl_include = [IO.Path]::Combine($root,"SDL2","2.0.22","include")
+$sdl_sys = [IO.Path]::Combine($ris_engine,"sdl-sys","src")
 
-write-host $headers
+$sdl_headers = Get-ChildItem -Path $sdl_include
 
-#write-host $PSScriptRoot
+foreach ($sdl_header in $sdl_headers)
+{
+    $source = $sdl_header.FullName
+    $target = [IO.Path]::Combine($sdl_sys, $sdl_header.Name)
+
+    # bindgen $source -o $target
+    Write-Host "$source > $target"
+}
