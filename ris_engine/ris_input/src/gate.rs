2fe69ba36ddbc05600dd58f6b85d1fca178de4c2
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-05-22 14:07:54 +0200

    implemented gate

---
+++ ris_engine/ris_input/src/gate.rs
@@ -0,0 +1,34 @@
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Gate{
+    up: bool,
+    down: bool,
+    hold:bool
+}
+
+impl Gate
+{
+    pub fn new() -> Gate{
+        Gate{up: false, down: false, hold: false}
+    }
+
+    pub fn update(&mut self, value: bool) {
+        self.up = !value && self.hold;
+        self.down = value && !self.hold;
+        self.hold = value;
+    }
+
+    pub fn up(&self) -> bool
+    {
+        self.up
+    }
+
+    pub fn down(&self) -> bool
+    {
+        self.down
+    }
+
+    pub fn hold(&self) -> bool
+    {
+        self.hold
+    }
+}
---
+++ ris_engine/ris_input/tests/gate_tests.rs
@@ -0,0 +1,45 @@
+use std::result;
+
+use ris_input::gate::Gate;
+
+#[test]
+fn should_create_correct_state()
+{
+    let mut gate = Gate::new();
+    let result0 = gate.clone();
+    
+    gate.update(true);
+    let result1 = gate.clone();
+    gate.update(false);
+    let result2 = gate.clone();
+    gate.update(false);
+    let result3 = gate.clone();
+    gate.update(true);
+    let result4 = gate.clone();
+    gate.update(true);
+    let result5 = gate.clone();
+
+    assert_eq!(result0.up(), false);
+    assert_eq!(result0.down(), false);
+    assert_eq!(result0.hold(), false);
+
+    assert_eq!(result1.up(), false);
+    assert_eq!(result1.down(), true);
+    assert_eq!(result1.hold(), true);
+
+    assert_eq!(result2.up(), true);
+    assert_eq!(result2.down(), false);
+    assert_eq!(result2.hold(), false);
+
+    assert_eq!(result3.up(), false);
+    assert_eq!(result3.down(), false);
+    assert_eq!(result3.hold(), false);
+
+    assert_eq!(result4.up(), false);
+    assert_eq!(result4.down(), true);
+    assert_eq!(result4.hold(), true);
+
+    assert_eq!(result5.up(), false);
+    assert_eq!(result5.down(), false);
+    assert_eq!(result5.hold(), true);
+}
