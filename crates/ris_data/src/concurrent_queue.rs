fae6f157a5552bd69ac73b8c49f0fa08a58df226
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-08-07 11:01:48 +0200

    working unsafe queue

--- crates/ris_data/src/concurrent_queue.rs
+++ crates/ris_data/src/concurrent_queue.rs
@@ -1,50 +1,49 @@
-
 pub struct ConcurrentQueue<T> {
-    head: Link<T>,
+    head: *mut Node<T>,
     tail: *mut Node<T>,
 }
 
-type Link<T> = Option<Box<Node<T>>>;
-
 struct Node<T> {
     data: T,
-    next: Link<T>,
+    next: *mut Node<T>,
 }
 
-impl<T> ConcurrentQueue<T> {
-    pub fn new() -> Self{
-        ConcurrentQueue { head: None, tail: std::ptr::null_mut() }
+impl<T> ConcurrentQueue<T>{
+    pub fn new() -> Self {
+        ConcurrentQueue { head: std::ptr::null_mut(), tail: std::ptr::null_mut() }
     }
 
     pub fn push(&mut self, data: T){
-        let mut new_tail = Box::new(Node {
+        let new_tail = Box::into_raw(Box::new(Node {
             data,
-            next: None,
-        });
-
-        let raw_tail: *mut _ = &mut *new_tail;
+            next: std::ptr::null_mut()
+        }));
 
         if !self.tail.is_null() {
             unsafe {
-                (*self.tail).next = Some(new_tail); // this leaks
+                (*self.tail).next = new_tail;
             }
         } else {
-            self.head = Some(new_tail);
+            self.head = new_tail;
         }
 
-        self.tail = raw_tail;
+        self.tail = new_tail;
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        self.head.take().map(|head| {
-            let head = *head;
+        if self.head.is_null() {
+            None
+        } else {
+            let head = unsafe {
+                Box::from_raw(self.head)
+            };
             self.head = head.next;
 
-            if self.head.is_none() {
+            if self.head.is_null() {
                 self.tail = std::ptr::null_mut();
             }
 
-            head.data
-        })
+            Some(head.data)
+        }
     }
 }
