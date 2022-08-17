0252846b69a94320cb7a0cb7088fdce0c685a71a
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-08-17 19:31:02 +0200

    fully implemented log

--- crates/ris_log/src/log_message.rs
+++ crates/ris_log/src/log_message.rs
@@ -1,6 +1,22 @@
+use std::fmt::write;
+
 use crate::constructed_log_message::ConstructedLogMessage;
 
 pub enum LogMessage {
     Constructed(ConstructedLogMessage),
     Plain(String),
+    ShutDown,
+}
+
+impl std::fmt::Display for LogMessage {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+
+        match self {
+            Self::Constructed(message) => write!(f, "{}", message)?,
+            Self::Plain(message) => write!(f, "{}", message)?,
+            Self::ShutDown => (),
+        };
+
+        Ok(())
+    }
 }
