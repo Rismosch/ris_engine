e15f084f133b7e817971b9c917f53fda8a965ef5
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-08-20 11:49:00 +0200

    job buffer works reliably on a single thread

--- crates/ris_jobs/src/job_buffer.rs
+++ crates/ris_jobs/src/job_buffer.rs
@@ -8,13 +8,6 @@ pub struct JobBuffer {
     jobs: Vec<Mutex<Option<Job>>>,
 }
 
-// pub type Job = Box<dyn FnOnce()>;
-
-pub enum PushResult {
-    Ok,
-    Full(Job),
-}
-
 impl JobBuffer {
     pub fn new(capacity: usize) -> Self {
         let mut jobs = Vec::with_capacity(capacity);
@@ -34,23 +27,23 @@ impl JobBuffer {
     //     panic!()
     // }
 
-    pub fn push(&mut self, job: Job) -> PushResult {
+    pub fn push(&mut self, job: Job) -> Result<(), Job> {
         let mut head = self.head.lock().unwrap();
         let old_head = head.clone();
         let mut node = self.jobs[old_head].lock().unwrap();
 
         match *node.deref() {
-            Some(_) => PushResult::Full(job),
+            Some(_) => Err(job),
             None => {
                 *node.deref_mut() = Some(job);
                 *head = (old_head + 1) % self.jobs.capacity();
 
-                PushResult::Ok
+                Ok(())
             }
         }
     }
 
-    pub fn pop(&mut self) -> Option<Job> {
+    pub fn pop(&mut self) -> Result<Job, ()> {
         let mut head = self.head.lock().unwrap();
         let old_head = head.clone();
         let new_head = if old_head == 0 {
@@ -62,27 +55,27 @@ impl JobBuffer {
         let mut node = self.jobs[new_head].lock().unwrap();
 
         match node.deref_mut().take() {
-            None => None,
+            None => Err(()),
             Some(job) => {
                 *head = new_head;
 
-                Some(job)
+                Ok(job)
             }
         }
     }
 
-    pub fn steal(&mut self) -> Option<Job> {
+    pub fn steal(&mut self) -> Result<Job, ()> {
         let mut tail = self.tail.lock().unwrap();
         let old_tail = tail.clone();
 
         let mut node = self.jobs[old_tail].lock().unwrap();
 
         match node.deref_mut().take() {
-            None => None,
+            None => Err(()),
             Some(job) => {
                 *tail = (old_tail + 1) % self.jobs.capacity();
 
-                Some(job)
+                Ok(job)
             }
         }
     }
--- tests/it/ris_jobs/job_buffer.rs
+++ tests/it/ris_jobs/job_buffer.rs
@@ -6,20 +6,17 @@ use ris_jobs::{job_buffer::JobBuffer, job::Job};
 fn should_push_and_pop() {
     let mut job_buffer = JobBuffer::new(4);
 
-    let magic_number = Rc::new(RefCell::new(0));
-    let job_magic_number = magic_number.clone();
-    let job = Job::new(move || {
-        *job_magic_number.borrow_mut() = 42;
-    });
+    let data = Rc::new(RefCell::new(0));
+    let moved_data = data.clone();
+    let job = Job::new(move || *moved_data.borrow_mut() = 42);
 
-    job_buffer.push(job);
+    let push = job_buffer.push(job);
+    assert!(push.is_ok());
+
     let job = job_buffer.pop();
-
-    assert!(job.is_some());
-
+    assert!(job.is_ok());
     job.unwrap().invoke();
-
-    assert_eq!(*magic_number.borrow(), 42);
+    assert_eq!(*data.borrow(), 42);
 }
 
 #[test]
@@ -27,31 +24,166 @@ fn should_push_and_steal()
 {
     let mut job_buffer = JobBuffer::new(4);
 
-    let magic_number = Rc::new(RefCell::new(0));
-    let job_magic_number = magic_number.clone();
-    let job = Job::new(move || {
-        *job_magic_number.borrow_mut() = 42;
-    });
+    let data = Rc::new(RefCell::new(0));
+    let moved_data = data.clone();
+    let job = Job::new(move || *moved_data.borrow_mut() = 42);
 
-    job_buffer.push(job);
+    let push = job_buffer.push(job);
+    assert!(push.is_ok());
+
     let job = job_buffer.steal();
-
-    assert!(job.is_some());
-
+    assert!(job.is_ok());
     job.unwrap().invoke();
-
-    assert_eq!(*magic_number.borrow(), 42);
+    assert_eq!(*data.borrow(), 42);
 }
 
-// #[test]
-// fn should_push_till_full()
-// {
+#[test]
+fn should_push_till_full()
+{
+    let mut job_buffer = JobBuffer::new(2);
 
-// }
+    let data = Rc::new(RefCell::new(0));
+    let moved_data1 = data.clone();
+    let moved_data2 = data.clone();
+    let moved_data3 = data.clone();
 
-// should_pop_till_empty
-// should_steal_till_empty
-// should_pop_and_steal_till_empty
+    let job1 = Job::new(move || *moved_data1.borrow_mut() = 1);
+    let job2 = Job::new(move || *moved_data2.borrow_mut() = 2);
+    let job3 = Job::new(move || *moved_data3.borrow_mut() = 3);
+
+    let push1 = job_buffer.push(job1);
+    let push2 = job_buffer.push(job2);
+    let push3 = job_buffer.push(job3);
+
+    assert!(push1.is_ok());
+    assert!(push2.is_ok());
+    assert!(push3.is_err());
+
+    push3.err().unwrap().invoke();
+    assert_eq!(*data.borrow(), 3);
+}
+
+#[test]
+fn should_pop_till_empty(){
+    let mut job_buffer = JobBuffer::new(4);
+
+    let data = Rc::new(RefCell::new(0));
+    let moved_data1 = data.clone();
+    let moved_data2 = data.clone();
+
+    let job1 = Job::new(move || *moved_data1.borrow_mut() = 1);
+    let job2 = Job::new(move || *moved_data2.borrow_mut() = 2);
+
+    let _ = job_buffer.push(job1);
+    let _ = job_buffer.push(job2);
+
+    let pop1 = job_buffer.pop();
+    let pop2= job_buffer.pop();
+    let pop3= job_buffer.pop();
+
+    assert!(pop1.is_ok());
+    assert!(pop2.is_ok());
+    assert!(pop3.is_err());
+
+    pop1.ok().unwrap().invoke();
+    assert_eq!(*data.borrow(), 2);
+    pop2.ok().unwrap().invoke();
+    assert_eq!(*data.borrow(), 1);
+}
+
+#[test]
+fn should_steal_till_empty(){
+    let mut job_buffer = JobBuffer::new(4);
+
+    let data = Rc::new(RefCell::new(0));
+    let moved_data1 = data.clone();
+    let moved_data2 = data.clone();
+
+    let job1 = Job::new(move || *moved_data1.borrow_mut() = 1);
+    let job2 = Job::new(move || *moved_data2.borrow_mut() = 2);
+
+    let _ = job_buffer.push(job1);
+    let _ = job_buffer.push(job2);
+
+    let steal1 = job_buffer.steal();
+    let steal2= job_buffer.steal();
+    let steal3= job_buffer.steal();
+
+    assert!(steal1.is_ok());
+    assert!(steal2.is_ok());
+    assert!(steal3.is_err());
+
+    steal1.ok().unwrap().invoke();
+    assert_eq!(*data.borrow(), 1);
+    steal2.ok().unwrap().invoke();
+    assert_eq!(*data.borrow(), 2);
+}
+
+#[test]
+fn should_push_pop_and_steal_multiple_times(){
+    let mut job_buffer = JobBuffer::new(5);
+
+    for _ in 0..5 {
+        let data = Rc::new(RefCell::new(0));
+        let moved_data1 = data.clone();
+        let moved_data2 = data.clone();
+        let moved_data3 = data.clone();
+        let moved_data4 = data.clone();
+        let moved_data5 = data.clone();
+        let moved_data6 = data.clone();
+        
+        let job1 = Job::new(move || *moved_data1.borrow_mut() = 1);
+        let job2 = Job::new(move || *moved_data2.borrow_mut() = 2);
+        let job3 = Job::new(move || *moved_data3.borrow_mut() = 3);
+        let job4 = Job::new(move || *moved_data4.borrow_mut() = 4);
+        let job5 = Job::new(move || *moved_data5.borrow_mut() = 5);
+        let job6 = Job::new(move || *moved_data6.borrow_mut() = 6);
+
+        let push1 = job_buffer.push(job1);
+        let push2 = job_buffer.push(job2);
+        let push3 = job_buffer.push(job3);
+        let push4 = job_buffer.push(job4);
+        let push5 = job_buffer.push(job5);
+        let push6 = job_buffer.push(job6);
+
+        assert!(push1.is_ok());
+        assert!(push2.is_ok());
+        assert!(push3.is_ok());
+        assert!(push4.is_ok());
+        assert!(push5.is_ok());
+        assert!(push6.is_err());
+
+        push6.err().unwrap().invoke();
+        assert_eq!(*data.borrow(), 6);
+
+        let steal1 = job_buffer.steal();
+        let pop2 = job_buffer.pop();
+        let steal3 = job_buffer.steal();
+        let pop4 = job_buffer.pop();
+        let steal5 = job_buffer.steal();
+        let pop6 = job_buffer.pop();
+        let steal7 = job_buffer.steal();
+
+        assert!(steal1.is_ok());
+        assert!(pop2.is_ok());
+        assert!(steal3.is_ok());
+        assert!(pop4.is_ok());
+        assert!(steal5.is_ok());
+        assert!(pop6.is_err());
+        assert!(steal7.is_err());
+
+        steal1.ok().unwrap().invoke();
+        assert_eq!(*data.borrow(), 1);
+        pop2.ok().unwrap().invoke();
+        assert_eq!(*data.borrow(), 5);
+        steal3.ok().unwrap().invoke();
+        assert_eq!(*data.borrow(), 2);
+        pop4.ok().unwrap().invoke();
+        assert_eq!(*data.borrow(), 4);
+        steal5.ok().unwrap().invoke();
+        assert_eq!(*data.borrow(), 3);
+    }
+}
 
 // should_push_and_pop_from_different_threads
 // should_push_on_empty_buffer_from_multiple_threads
