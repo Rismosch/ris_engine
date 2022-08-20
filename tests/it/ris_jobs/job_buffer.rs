d50d1b6eec30f1ff3412e750e138d1e22873911f
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-08-20 10:56:30 +0200

    basic job buffer works

--- tests/it/ris_jobs/job_buffer.rs
+++ tests/it/ris_jobs/job_buffer.rs
@@ -1,23 +1,57 @@
-use ris_jobs::job_buffer::JobBuffer;
+use std::{cell::RefCell, rc::Rc};
+
+use ris_jobs::{job_buffer::JobBuffer, job::Job};
 
 #[test]
-fn should_push_and_pop_on_a_single_thread() {
+fn should_push_and_pop() {
     let mut job_buffer = JobBuffer::new(4);
 
-    let magic_number = 42;
-    let job: Box<dyn FnMut()> = Box::new(move || {
-        assert_eq!(magic_number, 42);
+    let magic_number = Rc::new(RefCell::new(0));
+    let job_magic_number = magic_number.clone();
+    let job = Job::new(move || {
+        *job_magic_number.borrow_mut() = 42;
     });
-    // let job2: Box<dyn FnOnce()> = Box::new(|| {});
-    // let job3: Box<dyn FnOnce()> = Box::new(|| {});
 
     job_buffer.push(job);
     let job = job_buffer.pop();
 
     assert!(job.is_some());
 
-    job.unwrap()();
+    job.unwrap().invoke();
+
+    assert_eq!(*magic_number.borrow(), 42);
 }
+
+#[test]
+fn should_push_and_steal()
+{
+    let mut job_buffer = JobBuffer::new(4);
+
+    let magic_number = Rc::new(RefCell::new(0));
+    let job_magic_number = magic_number.clone();
+    let job = Job::new(move || {
+        *job_magic_number.borrow_mut() = 42;
+    });
+
+    job_buffer.push(job);
+    let job = job_buffer.steal();
+
+    assert!(job.is_some());
+
+    job.unwrap().invoke();
+
+    assert_eq!(*magic_number.borrow(), 42);
+}
+
+// #[test]
+// fn should_push_till_full()
+// {
+
+// }
+
+// should_pop_till_empty
+// should_steal_till_empty
+// should_pop_and_steal_till_empty
 
 // should_push_and_pop_from_different_threads
 // should_push_on_empty_buffer_from_multiple_threads
