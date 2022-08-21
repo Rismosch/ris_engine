84f48896bdce733bbe89383e235c45672f66e2f0
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-08-21 17:39:03 +0200

    refactoring and writing comments

--- crates/ris_jobs/src/errors.rs
+++ crates/ris_jobs/src/errors.rs
@@ -3,6 +3,10 @@ use std::fmt;
 use crate::job::Job;
 
 #[derive(Debug)]
+pub struct IsFull {
+    pub not_pushed_job: Job,
+}
+#[derive(Debug)]
 pub struct IsEmpty;
 
 impl fmt::Display for IsEmpty {
@@ -11,17 +15,11 @@ impl fmt::Display for IsEmpty {
     }
 }
 
-impl std::error::Error for IsEmpty {}
-
-#[derive(Debug)]
-pub struct IsFull {
-    pub not_pushed_job: Job,
-}
-
 impl fmt::Display for IsFull {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "is full")
     }
 }
 
+impl std::error::Error for IsEmpty {}
 impl std::error::Error for IsFull {}
