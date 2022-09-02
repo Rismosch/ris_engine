33d721fccc013e7f925ecc7623ac98142597cd45
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-09-02 07:49:44 +0200

    job future is now safe

--- crates/ris_jobs/src/job_future.rs
+++ crates/ris_jobs/src/job_future.rs
@@ -1,77 +1,45 @@
 use std::{
     ptr::NonNull,
-    sync::atomic::{AtomicBool, Ordering},
-    task::Poll,
+    sync::{atomic::{AtomicBool, Ordering}, Arc, Mutex},
+    task::Poll, cell::RefCell,
 };
 
-pub struct SettableJobFuture<T: Clone> {
-    inner: NonNull<Inner<T>>,
+type Data<T> = Arc<Mutex<Poll<T>>>;
+
+pub struct SettableJobFuture<T> {
+    data: Data<T>,
 }
 
-pub struct JobFuture<T: Clone> {
-    inner: NonNull<Inner<T>>,
+pub struct JobFuture<T> {
+    data: Data<T>,
 }
 
-pub struct Inner<T> {
-    poll: Poll<T>,
-    can_be_dropped: AtomicBool,
-}
-
-impl<T: Clone> SettableJobFuture<T> {
+impl<T> SettableJobFuture<T> {
     pub fn new() -> (SettableJobFuture<T>, JobFuture<T>) {
-        let boxed = Box::into_raw(Box::new(Inner {
-            poll: Poll::Pending,
-            can_be_dropped: AtomicBool::new(false),
-        }));
+        let data = Arc::new(Mutex::new(Poll::Pending));
 
-        let inner = unsafe { NonNull::new_unchecked(boxed) };
-
-        let settable_job_future = SettableJobFuture { inner };
-        let job_future = JobFuture { inner };
+        let settable_job_future = SettableJobFuture { data: data.clone() };
+        let job_future = JobFuture { data };
 
         (settable_job_future, job_future)
     }
 
     pub fn set(&mut self, result: T) {
-        let inner = unsafe { &mut *self.inner.as_ptr() };
-        inner.poll = Poll::Ready(result);
+        self.data.lock().map_or_else(
+            |e| ris_log::error!("could not lock future: {}", e),
+            |mut data| *data = Poll::Ready(result)
+        );
     }
 }
 
 impl<T: Clone> JobFuture<T> {
-    pub fn poll(&self) -> &Poll<T> {
-        let inner = unsafe { &*self.inner.as_ptr() };
-
-        &inner.poll
+    pub fn poll(&self) -> Poll<T> {
+        self.data.lock().map_or_else(
+            |e| {
+                ris_log::error!("could not lock future: {}", e);
+                Poll::Pending
+            },
+            |data| data.clone()
+        )
     }
 }
-
-impl<T: Clone> Drop for SettableJobFuture<T> {
-    fn drop(&mut self) {
-        let inner = unsafe { &mut *self.inner.as_ptr() };
-
-        drop_inner(inner);
-    }
-}
-
-impl<T: Clone> Drop for JobFuture<T> {
-    fn drop(&mut self) {
-        let inner = unsafe { &mut *self.inner.as_ptr() };
-
-        drop_inner(inner);
-    }
-}
-
-fn drop_inner<T>(inner: &mut Inner<T>) {
-    let can_be_dropped = inner.can_be_dropped.fetch_or(true, Ordering::SeqCst);
-    if can_be_dropped {
-        unsafe {
-            Box::from_raw(inner);
-        }
-    }
-}
-
-unsafe impl<T: Clone> Send for SettableJobFuture<T> {}
-unsafe impl<T: Clone> Sync for SettableJobFuture<T> {}
-unsafe impl<T: Clone> Send for JobFuture<T> {}
-unsafe impl<T: Clone> Sync for JobFuture<T> {}
