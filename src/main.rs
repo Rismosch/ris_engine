cf86cb83584033d19d312af76242709c78a37c8d
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-09-02 19:57:21 +0200

    small improvements

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use ris_core::engine::Engine;
 use ris_data::info::package_info::PackageInfo;
 use ris_data::package_info;
 use ris_log::{
-    appenders::{console_appender::ConsoleAppender, i_appender::IAppender},
+    appenders::console_appender::ConsoleAppender,
     log::{self, Appenders},
     log_level::LogLevel,
 };
 
 fn main() -> Result<(), String> {
-    let appenders: Appenders  = vec![ConsoleAppender::new()];
+    let appenders: Appenders = vec![ConsoleAppender::new()];
     let log_guard = log::init(LogLevel::Trace, appenders, false);
 
     let package_info = package_info!();
