d14201dd07dd0fa6451b8d2852aa2281444c8249
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-09-04 21:37:54 +0200

    further work on frame buffer

--- crates/ris_core/src/god_job.rs
+++ crates/ris_core/src/god_job.rs
@@ -1,28 +1,45 @@
-use std::{thread, time::Duration};
+use std::{thread, time::{Duration, Instant}};
 
-use ris_data::gameloop::gameloop_state::GameloopState;
+use ris_data::gameloop::{gameloop_state::GameloopState, input_data::{InputData, self}, logic_data::LogicData, output_data::OutputData, frame_data::FrameData};
 use ris_jobs::job_system;
 
 use crate::{
-    god_object::GodObject, gameloop::{output_frame::run_output, logic_frame::run_logic},
+    god_object::GodObject, gameloop::{output_frame::run_output, logic_frame::run_logic, input_frame::run_input},
 };
 
 pub fn run(god_object: &mut GodObject) -> Result<(), String> {
-    start_up(god_object);
-    main_loop(god_object)
-}
 
-fn start_up(god_object: &mut GodObject) {
+    let mut current_input = InputData::default();
+    let mut current_logic = LogicData::default();
+    let mut current_output = OutputData::default();
+    let previous_input = InputData::default();
+    let previous_logic = LogicData::default();
+    let previous_output = OutputData::default();
 
-}
-
-fn main_loop(god_object: &mut GodObject) -> Result<(), String> {
     // loop {
-    //     let output_future = job_system::submit(|| run_output());
 
-    //     let logic_future = job_system::submit(|| run_logic());
+    //     let frame_data = FrameData::new();
 
-    //     // input
+    //     let previous_logic_output = &previous_logic;
+    //     let output_future = job_system::submit(move ||run_output(
+    //         &mut current_output,
+    //         &previous_output,
+    //         &previous_logic_output,
+    //         &frame_data
+    //     ));
+
+    //     let logic_future = job_system::submit(|| run_logic(
+    //         &mut current_logic,
+    //         &previous_logic,
+    //         &previous_input,
+    //         &frame_data
+    //     ));
+
+    //     run_input(
+    //         &mut current_input,
+    //         &previous_input,
+    //         &frame_data
+    //     );
 
     //     job_system::wait(output_future);
     //     let gameloop_state = job_system::wait(logic_future);
--- crates/ris_data/src/gameloop/gameloop_state.rs
+++ crates/ris_data/src/gameloop/gameloop_state.rs
@@ -1,3 +1,4 @@
+#[derive(Clone)]
 pub enum GameloopState {
     WantsToContinue,
     WantsToQuit,
