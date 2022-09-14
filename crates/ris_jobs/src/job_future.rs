c4775a4e041bc4b7269430adc0bed7bf226b319d
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-09-14 19:51:13 +0200

    job future is now based on atomics, not on mutexes

--- crates/ris_jobs/src/job_future.rs
+++ crates/ris_jobs/src/job_future.rs
@@ -1,68 +1,96 @@
-use std::sync::{Arc, Mutex, TryLockError};
+use std::{sync::atomic::{AtomicBool, AtomicI32, Ordering}, ptr::NonNull};
 
-use crate::{job_poll::JobPoll, job_system};
+use crate::job_system;
 
-type Data<T> = Arc<Mutex<JobPoll<T>>>;
+struct Inner<T> {
+    is_ready: AtomicBool,
+    data: Option<T>,
+    refs: AtomicI32,
+}
+
+type InnerPtr<T> = NonNull<Inner<T>>;
 
 pub struct SettableJobFuture<T> {
-    data: Data<T>,
+    inner: InnerPtr<T>,
 }
 
 #[must_use]
 pub struct JobFuture<T> {
-    data: Data<T>,
+    inner: InnerPtr<T>,
 }
 
 impl<T> SettableJobFuture<T> {
     pub fn new() -> (SettableJobFuture<T>, JobFuture<T>) {
-        let data = Arc::new(Mutex::new(JobPoll::Pending));
 
-        let settable_job_future = SettableJobFuture { data: data.clone() };
-        let job_future = JobFuture { data };
+        let boxed = Box::into_raw(Box::new(Inner{
+            is_ready: AtomicBool::new(false),
+            data: None,
+            refs: AtomicI32::new(1),
+        }));
+
+        let inner_a = unsafe {NonNull::new_unchecked(boxed)};
+        let inner_b = unsafe {NonNull::new_unchecked(boxed)};
+
+        let settable_job_future = SettableJobFuture { inner: inner_a };
+        let job_future = JobFuture { inner: inner_b };
 
         (settable_job_future, job_future)
     }
 
-    pub fn set(self, result: T) {
-        self.data.lock().map_or_else(
-            |e| ris_log::error!("could not lock future: {}", e),
-            |mut data| *data = JobPoll::Ready(Some(result)),
-        );
+    pub fn set(mut self, result: T) {
+        let inner = deref_inner(&mut self.inner);
+        inner.data = Some(result);
+        inner.is_ready.store(true, Ordering::SeqCst);
     }
 }
 
 impl<T> JobFuture<T> {
-    pub fn wait(self) -> T {
+    pub fn wait(mut self) -> T {
         match self.wait_and_take() {
             Some(value) => value,
             None => unreachable!(),
         }
     }
 
-    fn wait_and_take(&self) -> Option<T> {
-        loop {
-            match self.data.try_lock() {
-                Ok(mut data) => {
-                    if data.is_ready() {
-                        return data.take();
-                    }
-                }
-                Err(e) => {
-                    if let TryLockError::Poisoned(e) = e {
-                        let message = format!("could not lock future: {}", e);
-                        ris_log::error!("{}", message);
-                        panic!("{}", message);
-                    }
-                }
-            }
+    fn wait_and_take(&mut self) -> Option<T> {
+        let inner = deref_inner(&mut self.inner);
 
+        while !inner.is_ready.load(Ordering::SeqCst) {
             job_system::run_pending_job();
         }
+
+        inner.data.take()
+    }
+}
+
+impl<T> Drop for SettableJobFuture<T> {
+    fn drop(&mut self) {
+        drop_inner(&mut self.inner);
     }
 }
 
 impl<T> Drop for JobFuture<T> {
     fn drop(&mut self) {
         self.wait_and_take();
+
+        drop_inner(&mut self.inner);
+    }
+}
+
+unsafe impl<T> Send for SettableJobFuture<T> {}
+unsafe impl<T> Send for JobFuture<T> {}
+
+fn deref_inner<T>(data: &mut InnerPtr<T>) -> &mut Inner<T> {
+    unsafe {&mut *data.as_ptr()}
+}
+
+fn drop_inner<T>(inner: &mut InnerPtr<T>) {
+    let inner = deref_inner(inner);
+
+    let previous_refs = inner.refs.fetch_sub(1, Ordering::SeqCst);
+    if previous_refs < 1 {
+        unsafe {
+            Box::from_raw(inner);
+        }
     }
 }
