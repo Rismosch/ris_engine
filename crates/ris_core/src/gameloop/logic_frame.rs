f10bfb31a58acb147298ec7c293227f4d5040400
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-09-14 19:51:13 +0200

    job future is now based on atomics, not on mutexes

--- crates/ris_core/src/gameloop/logic_frame.rs
+++ crates/ris_core/src/gameloop/logic_frame.rs
@@ -1,7 +1,10 @@
+// use std::{thread, time::Duration};
+
 use ris_data::gameloop::{
     frame_data::FrameData, gameloop_state::GameloopState, input_data::InputData,
     logic_data::LogicData,
 };
+// use ris_jobs::job_system;
 
 pub fn run(
     current: LogicData,
@@ -11,7 +14,7 @@ pub fn run(
 ) -> (LogicData, GameloopState) {
     // thread::sleep(Duration::from_millis(50));
 
-    // ris_log::debug!("{:#034b} {} {}", input.get_keyboard().buttons.down(), job_system::thread_index(), frame.fps());
+    // ris_log::debug!("{:#034b} {} {}", _input.get_keyboard().buttons.down(), job_system::thread_index(), _frame.fps());
 
     (current, GameloopState::WantsToContinue)
 }
--- crates/ris_jobs/src/lib.rs
+++ crates/ris_jobs/src/lib.rs
@@ -2,5 +2,4 @@ pub mod errors;
 pub mod job;
 pub mod job_buffer;
 pub mod job_future;
-pub mod job_poll;
 pub mod job_system;
