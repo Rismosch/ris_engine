19cb6853c9c5a454fe116fcd6528ce389bacf981
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-09-14 19:57:54 +0200

    found a better way to move ownership in and out of references

--- crates/ris_data/src/gameloop/input_data.rs
+++ crates/ris_data/src/gameloop/input_data.rs
@@ -1,53 +1,7 @@
 use crate::input::{keyboard_data::KeyboardData, mouse_data::MouseData};
 
+#[derive(Default)]
 pub struct InputData {
-    mouse: Option<MouseData>,
-    keyboard: Option<KeyboardData>,
-}
-
-impl InputData {
-    pub fn get_mouse(&self) -> &MouseData {
-        match &self.mouse {
-            Some(data) => data,
-            None => panic!("mouse was taken"),
-        }
-    }
-
-    pub fn take_mouse(&mut self) -> MouseData {
-        match self.mouse.take() {
-            Some(data) => data,
-            None => panic!("mouse was taken"),
-        }
-    }
-
-    pub fn set_mouse(&mut self, data: MouseData) {
-        self.mouse = Some(data);
-    }
-
-    pub fn get_keyboard(&self) -> &KeyboardData {
-        match &self.keyboard {
-            Some(data) => data,
-            None => panic!("keyboard was taken"),
-        }
-    }
-
-    pub fn take_keyboard(&mut self) -> KeyboardData {
-        match self.keyboard.take() {
-            Some(data) => data,
-            None => panic!("keyboard was taken"),
-        }
-    }
-
-    pub fn set_keyboard(&mut self, data: KeyboardData) {
-        self.keyboard = Some(data);
-    }
-}
-
-impl Default for InputData {
-    fn default() -> Self {
-        Self {
-            mouse: Some(MouseData::default()),
-            keyboard: Some(KeyboardData::default()),
-        }
-    }
+    pub mouse: MouseData,
+    pub keyboard: KeyboardData,
 }
