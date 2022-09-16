9c7ea1f9ec507b4edc8b136816c4d1c4a08d85a1
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-09-16 08:16:02 +0200

    further work on affinity

--- crates/ris_os/src/affinity/windows.rs
+++ crates/ris_os/src/affinity/windows.rs
@@ -1,8 +1,18 @@
-pub fn set_affinity(core_ids: &[usize]) {
-    ris_log::debug!("set affinity {:?}", core_ids);
-}
+use std::ffi::c_void;
 
-pub fn get_affinity() -> Vec<usize> {
-    ris_log::debug!("get affinity");
-    Vec::new()
+pub fn set_affinity(core_ids: &[usize]) {
+    let current_thread = unsafe { GetCurrentThread() };
+    let mut mask = 0usize;
+
+    for core_id in core_ids {
+        mask |= 1usize << core_id;
+    }
+
+    let result = unsafe { SetThreadAffinityMask(current_thread, mask) };
+    if result == 0 {
+        let error = unsafe { GetLastError() };
+        ris_log::error!("SetThreadAffinityMask failed with error 0x{:x}", error);
+    } else {
+        ris_log::debug!("set affinity {:?}", core_ids);
+    }
 }
