edb720c4fcc1f68f1f95fcb52519387c459d883b
Rismosch Desktop <simon.sutoris@googlemail.com>
2022-09-19 20:33:09 +0200

    further work on jobcell

--- tests/it/ris_jobs/job_cell.rs
+++ tests/it/ris_jobs/job_cell.rs
@@ -1,61 +1,73 @@
-use ris_jobs::job_cell::JobCell;
+use core::borrow;
+use std::sync::{Mutex, Arc};
+
+use ris_jobs::{job_cell::{JobCell, self}, job_system};
 
 #[test]
-fn should_burrow() {
-    let job_cell = JobCell::new(42);
+fn should_deref() {
+    let mut job_cell = JobCell::new(42);
 
-    let borrow1 = job_cell.borrow();
-    let borrow2 = job_cell.borrow();
-    let borrow3 = job_cell.borrow();
+    let a = *job_cell;
+    *job_cell = 13;
+    let b = *job_cell;
 
-    assert_eq!(*borrow1, 42);
-    assert_eq!(*borrow2, 42);
-    assert_eq!(*borrow3, 42);
+    assert_eq!(a, 42);
+    assert_eq!(b, 13);
 }
 
 #[test]
-fn should_clone_burrow() {
+fn should_reference_and_clone() {
     let job_cell = JobCell::new(42);
 
-    let borrow1 = job_cell.borrow();
-    let borrow2 = borrow1.clone();
-    let borrow3 = borrow2.clone();
+    let ref_cell = job_cell.ref_cell();
 
-    assert_eq!(*borrow1, 42);
-    assert_eq!(*borrow2, 42);
-    assert_eq!(*borrow3, 42);
+    let ref1 = ref_cell.borrow();
+    let ref2 = ref_cell.borrow();
+    let ref3 = ref_cell.borrow();
+    let clone1 = ref1.clone();
+    let clone2 = ref2.clone();
+    let clone3 = ref3.clone();
+
+    assert_eq!(*ref1, 42);
+    assert_eq!(*ref2, 42);
+    assert_eq!(*ref3, 42);
+    assert_eq!(*clone1, 42);
+    assert_eq!(*clone2, 42);
+    assert_eq!(*clone3, 42);
 }
 
 #[test]
-fn should_burrow_mut() {
+fn should_returning_to_cell() {
     let job_cell = JobCell::new(42);
+    let ref_cell =  job_cell.ref_cell();
+    let job_cell = ref_cell.return_cell();
 
-    {
-        let mut borrow_mut = job_cell.borrow_mut();
-        *borrow_mut = -13;
+    assert_eq!(*job_cell, 42);
+}
+
+#[test]
+fn should_run_jobs_when_returning_to_cell() {
+    let job_system = job_system::init(100, 100);
+    let results = Arc::new(Mutex::new(Vec::new()));
+
+    let mut job_cell = JobCell::default();
+
+    for i in 0..100 {
+        *job_cell = i;
+        let ref_cell = job_cell.ref_cell();
+
+        for _ in 0..100 {
+            let borrowed = ref_cell.borrow();
+            let results_copy = results.clone();
+            job_system::submit(move||{
+                results_copy.lock().unwrap().push(*borrowed);
+            });
+        }
+
+        job_cell = ref_cell.return_cell();
     }
 
-    let borrow = job_cell.borrow();
+    let 
 
-    assert_eq!(*borrow, -13);
-}
-
-#[test]
-fn should_run_jobs_when_burrowing_mut_when_already_burrowed() {
-    panic!()
-}
-
-#[test]
-fn should_run_jobs_when_burrowing_mut_when_already_burrowed_mut() {
-    panic!()
-}
-
-#[test]
-fn should_run_jobs_when_replacing_when_already_burrowed() {
-    panic!()
-}
-
-#[test]
-fn should_run_jobs_when_replacing_when_already_burrowed_mut() {
-    panic!()
+    drop(job_system);
 }
