a606d06ee0b2674d64e9a6dfc519c9b14473936b
Rismosch Desktop <simon.sutoris@googlemail.com>
2023-08-02 17:54:51 +0200

    implemented ci script to move docs, such that `cargo clean` doesn't kill the docs

--- ci/doc.ps1
+++ ci/doc.ps1
@@ -5,13 +5,22 @@ Write-Host "creating documentation..."
 
 cargo doc
 
-if ($LASTEXITCODE) {
+if ($LASTEXITCODE -eq 0) {
     Write-Host "cargo doc succeeded!"
     Write-Host "clearing destination directory..."
 
-    $final_directory = GetAndClearCiOutDir
+    $target_directory = GetAndClearCiOutDir
+    $source_directory = "$root_dir/target/doc"
 
+    Write-Host "copying documentation..."
 
+    Copy-Item -Path "$source_directory/*" -Destination $target_directory -Recurse
+
+    Write-Host "opening..."
+
+    start "$target_directory/ris_engine/index.html"
+
+    Write-Host "done! final documentation can be found unfer ``$target_directory``"
 } else {
     Write-Host "cargo doc was unsuccessful"
 }
