95c3476374e00d8c67fd3118f353788c75b8b4e7
Rismosch Desktop <simon.sutoris@googlemail.com>
2023-08-08 21:40:59 +0200

    implemented clean

--- ci/archive.ps1
+++ ci/archive.ps1
@@ -15,13 +15,13 @@ $cli_clean = "--clean"
 $cli_no_clean = "--no-clean"
 $cli_clean_value = $false
 
-$cli_include_git = "--include-git"
-$cli_no_include_git = "--no-include-git"
-$cli_include_git_value = $false
-
 $cli_vendor = "--vendor"
 $cli_no_vendor = "--no-vendor"
 $cli_vendor_value = $false
+
+$cli_include_git = "--include-git"
+$cli_no_include_git = "--no-include-git"
+$cli_include_git_value = $false
 
 if ($args.length -eq 0) {
     Write-Host ""
@@ -33,11 +33,11 @@ if ($args.length -eq 0) {
     Write-Host "    $cli_clean           cleans the workspace by running a combination of `git` commands"
     Write-Host "    $cli_no_clean        does not clean the workspace (default)"
     Write-Host ""
-    Write-Host "    $cli_include_git     include the `./.git` directory in the resulting archive"
-    Write-Host "    $cli_no_include_git  does not include the `./.git` directory in the resulting archive (default)"
-    Write-Host ""
     Write-Host "    $cli_vendor          downloads dependencies using `cargo vendor` and prepares the workspace accordingly"
     Write-Host "    $cli_no_vendor       does not download dependencies (default)"
+    Write-Host ""
+    Write-Host "    $cli_include_git     include the `./.git` directory in the resulting archive"
+    Write-Host "    $cli_no_include_git  does not include the `./.git` directory in the resulting archive (default)"
     Write-Host ""
     Write-Host "Default values are chosen to make minimal to no changes to the workspace. I recommend calling this script with these settings:"
     Write-Host "    $cli_clean"
@@ -54,14 +54,14 @@ if ($args.length -eq 0) {
         $cli_clean_value = $true
     }
 
-    $user_input = Read-Host "should the `./.git` directory should be included in the resulting archive? (y/N)"
-    if ($user_input.ToLower() -eq "y") {
-        $cli_include_git_value = $true
-    }
-
     $user_input = Read-Host "should dependencies be downloaded? (y/N)"
     if ($user_input.ToLower() -eq "y") {
         $cli_vendor_value = $true
+    }
+
+    $user_input = Read-Host "should the `./.git` directory should be included in the resulting archive? (y/N)"
+    if ($user_input.ToLower() -eq "y") {
+        $cli_include_git_value = $true
     }
 } else {
     for($i = 0; $i -lt $args.length; ++$i) {
@@ -79,6 +79,18 @@ if ($args.length -eq 0) {
     }
 }
 
-Write-Host $cli_clean_value
-Write-Host $cli_include_git_value
+if ($cli_clean_value -eq $true) {
+    Write-Host "cleaning workspace..."
+
+    Write-Host "git reset ."
+    git reset .
+
+    Write-Host "git checkout -- ."
+    git checkout -- .
+
+    Write-Host "git clean -dxf"
+    git clean -dxf
+}
+
 Write-Host $cli_vendor_value
+Write-Host $cli_include_git_value
