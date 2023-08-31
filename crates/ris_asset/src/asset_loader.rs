08dfcded01cc8cb9226cedc7f5cf5521e7b50f67
Simons Thinkpad <simon.sutoris@googlemail.com>
2023-08-31 23:52:27 +0200

    started work on asset loader

--- crates/ris_asset/src/asset_loader.rs
+++ crates/ris_asset/src/asset_loader.rs
@@ -4,24 +4,32 @@ use std::path::PathBuf;
 use ris_data::info::app_info::AppInfo;
 use ris_jobs::job_future::JobFuture;
 use ris_jobs::job_future::SettableJobFuture;
+use ris_util::ris_error::RisError;
 
 use crate::asset_id::AssetId;
 
 pub struct AssetLoader {}
 
 impl AssetLoader {
-    pub fn new(app_info: &AppInfo) -> Self {
-        let args_assets_path = String::from(app_info.args.assets);
+    pub fn new(app_info: &AppInfo) -> Result<Self, RisError> {
+
+        let asset_path;
 
         // check if path is relative
-        let mut asset_path = PathBuf::new();
-        asset_path.push(&app_info.file.base_path);
-        asset_path.push();
+        let mut relative_path = PathBuf::new();
+        relative_path.push(&app_info.file.base_path);
+        relative_path.push(String::from(&app_info.args.assets));
+        let relative_path = Path::new(&relative_path);
+        if relative_path.exists() {
+            asset_path = relative_path;
+        } else {
+
+        }
 
         // check if path is absolute
-        
 
-        Self {}
+
+        Ok(Self {})
     }
 
     //pub fn load(id: AssetId) -> JobFuture<Box<[u8]>> {
