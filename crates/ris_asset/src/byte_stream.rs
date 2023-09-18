3d9d62764528a448e38d4ebacd45014566d44ee3
Simons Thinkpad <simon.sutoris@googlemail.com>
2023-09-18 23:40:34 +0200

    cargo clippy fmt

--- crates/ris_asset/src/byte_stream.rs
+++ crates/ris_asset/src/byte_stream.rs
@@ -1,8 +1,8 @@
 use std::io::Read;
 use std::io::Result;
-use std::io::Write;
 use std::io::Seek;
 use std::io::SeekFrom;
+use std::io::Write;
 
 pub struct ByteStream {
     bytes: Vec<u8>,
@@ -11,10 +11,7 @@ pub struct ByteStream {
 
 impl ByteStream {
     pub fn new(bytes: Vec<u8>) -> Self {
-        Self {
-            bytes,
-            pointer: 0,
-        }
+        Self { bytes, pointer: 0 }
     }
 
     pub fn to_bytes(self) -> Vec<u8> {
@@ -25,16 +22,16 @@ impl ByteStream {
 impl Read for ByteStream {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
         let remaining_bytes = self.bytes.len() - self.pointer;
-        let byte_count = usize::min(remaining_bytes, buf.len());
+        let count = usize::min(remaining_bytes, buf.len());
 
-        for i in 0..byte_count {
+        for (i, item) in buf.iter_mut().enumerate().take(count) {
             let pointer = self.pointer + i;
-            buf[i] = self.bytes[pointer];
+            *item = self.bytes[pointer];
         }
 
-        self.pointer += byte_count;
+        self.pointer += count;
 
-        Ok(byte_count)
+        Ok(count)
     }
 }
 
@@ -44,16 +41,13 @@ impl Write for ByteStream {
         let overwrite = usize::min(existing, buf.len());
         let remaining = buf.len() - overwrite;
 
-        println!("hoi {} {}", overwrite, remaining);
-
-        for i in 0..overwrite {
+        for (i, item) in buf.iter().enumerate().take(overwrite) {
             let pointer = self.pointer + i;
-            self.bytes[pointer] = buf[i]
+            self.bytes[pointer] = *item;
         }
 
-        for i in 0..remaining {
-            let pointer = overwrite + i;
-            self.bytes.push(buf[pointer]);
+        for item in buf.iter().skip(overwrite).take(remaining) {
+            self.bytes.push(*item);
         }
 
         self.pointer += buf.len();
@@ -69,15 +63,9 @@ impl Write for ByteStream {
 impl Seek for ByteStream {
     fn seek(&mut self, pos: SeekFrom) -> Result<u64> {
         let pointer = match pos {
-            SeekFrom::Start(pos) => {
-                pos as isize
-            },
-            SeekFrom::End(pos) => {
-                self.bytes.len() as isize + pos as isize
-            },
-            SeekFrom::Current(pos) => {
-                self.pointer as isize + pos as isize
-            },
+            SeekFrom::Start(pos) => pos as isize,
+            SeekFrom::End(pos) => self.bytes.len() as isize + pos as isize,
+            SeekFrom::Current(pos) => self.pointer as isize + pos as isize,
         };
 
         self.pointer = if pointer < 0 {
--- tests/it/ris_asset/byte_stream.rs
+++ tests/it/ris_asset/byte_stream.rs
@@ -1,14 +1,14 @@
 use std::io::Read;
-use std::io::Write;
 use std::io::Seek;
 use std::io::SeekFrom;
+use std::io::Write;
 
 use ris_asset::byte_stream::ByteStream;
 use ris_asset::util;
 
 #[test]
 fn should_seek_from_start() {
-    let bytes = vec![0,1,2,3,4,5,6,7,8,9];
+    let bytes = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     let mut stream = ByteStream::new(bytes);
 
     let result = stream.seek(SeekFrom::Start(3)).unwrap();
@@ -25,7 +25,7 @@ fn should_seek_from_start() {
 
 #[test]
 fn should_seek_from_end() {
-    let bytes = vec![0,1,2,3,4,5,6,7,8,9];
+    let bytes = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     let mut stream = ByteStream::new(bytes);
 
     let result = stream.seek(SeekFrom::End(-5)).unwrap();
@@ -42,7 +42,7 @@ fn should_seek_from_end() {
 
 #[test]
 fn should_seek_from_current() {
-    let bytes = vec![0,1,2,3,4,5,6,7,8,9];
+    let bytes = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     let mut stream = ByteStream::new(bytes);
 
     let result = stream.seek(SeekFrom::Current(-5)).unwrap();
@@ -61,31 +61,31 @@ fn should_seek_from_current() {
 
 #[test]
 fn should_read() {
-    let bytes = vec![0,1,2,3,4,5,6,7,8,9];
+    let bytes = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     let mut stream = ByteStream::new(bytes);
 
     let mut result = [0; 10];
     let count = stream.read(&mut result).unwrap();
     assert_eq!(count, 10);
-    assert!(util::bytes_equal(&result, &[0,1,2,3,4,5,6,7,8,9]));
+    assert!(util::bytes_equal(&result, &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
 
     stream.seek(SeekFrom::Start(3)).unwrap();
     let mut result = [0; 5];
     let count = stream.read(&mut result).unwrap();
     assert_eq!(count, 5);
-    assert!(util::bytes_equal(&result, &[3,4,5,6,7]));
+    assert!(util::bytes_equal(&result, &[3, 4, 5, 6, 7]));
 
     stream.seek(SeekFrom::End(0)).unwrap();
     let mut result = [0; 5];
     let count = stream.read(&mut result).unwrap();
     assert_eq!(count, 0);
-    assert!(util::bytes_equal(&result, &[0,0,0,0,0]));
-    
+    assert!(util::bytes_equal(&result, &[0, 0, 0, 0, 0]));
+
     stream.seek(SeekFrom::End(-2)).unwrap();
     let mut result = [0; 5];
     let count = stream.read(&mut result).unwrap();
     assert_eq!(count, 2);
-    assert!(util::bytes_equal(&result, &[8,9,0,0,0]));
+    assert!(util::bytes_equal(&result, &[8, 9, 0, 0, 0]));
 }
 
 #[test]
@@ -101,36 +101,36 @@ fn should_write_nothing() {
 
 #[test]
 fn should_append_on_write() {
-    let bytes = vec![0,1,2,3,4];
+    let bytes = vec![0, 1, 2, 3, 4];
     let mut stream = ByteStream::new(bytes);
 
     stream.seek(SeekFrom::End(0)).unwrap();
-    let count = stream.write(&[5,6,7,8,9]).unwrap();
+    let count = stream.write(&[5, 6, 7, 8, 9]).unwrap();
     let result = stream.to_bytes();
     assert_eq!(count, 5);
-    assert!(util::bytes_equal(&result, &[0,1,2,3,4,5,6,7,8,9]));
+    assert!(util::bytes_equal(&result, &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
 }
 
 #[test]
 fn should_overwrite() {
-    let bytes = vec![0,1,2,3,4,5,6,7,8,9];
+    let bytes = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     let mut stream = ByteStream::new(bytes);
 
     stream.seek(SeekFrom::Start(3)).unwrap();
-    let count = stream.write(&[5,6,7,8,9]).unwrap();
+    let count = stream.write(&[5, 6, 7, 8, 9]).unwrap();
     let result = stream.to_bytes();
     assert_eq!(count, 5);
-    assert!(util::bytes_equal(&result, &[0,1,2,5,6,7,8,9,8,9]));
+    assert!(util::bytes_equal(&result, &[0, 1, 2, 5, 6, 7, 8, 9, 8, 9]));
 }
 
 #[test]
 fn should_overwrite_and_append() {
-    let bytes = vec![0,1,2,3,4];
+    let bytes = vec![0, 1, 2, 3, 4];
     let mut stream = ByteStream::new(bytes);
 
     stream.seek(SeekFrom::Start(2)).unwrap();
-    let count = stream.write(&[5,6,7,8,9]).unwrap();
+    let count = stream.write(&[5, 6, 7, 8, 9]).unwrap();
     let result = stream.to_bytes();
     assert_eq!(count, 5);
-    assert!(util::bytes_equal(&result, &[0,1,5,6,7,8,9]), "result: {:?}", result);
+    assert!(util::bytes_equal(&result, &[0, 1, 5, 6, 7, 8, 9]));
 }
