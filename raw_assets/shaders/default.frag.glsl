42e424974ad25e5ebb76cbc6e63680356587051e
Rismosch Desktop <simon.sutoris@googlemail.com>
2023-10-03 22:09:22 +0200

    refactoring shaders

--- raw_assets/shaders/default.frag.glsl
+++ raw_assets/shaders/default.frag.glsl
@@ -1,10 +1,10 @@
 #version 460
 #pragma shader_stage(fragment)
 
-layout(location = 0) in vec3 fragColor;
+layout(location = 0) in vec3 frag_color;
 
-layout(location = 0) out vec4 f_color;
+layout(location = 0) out vec4 out_color;
 
 void main() {
-    f_color = vec4(fragColor, 1.0);
+    out_color = vec4(frag_color, 1.0);
 }
--- raw_assets/shaders/default.vert.glsl
+++ raw_assets/shaders/default.vert.glsl
@@ -10,10 +10,11 @@ layout(set = 0, binding = 0) uniform UniformBufferObject {
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec3 color;
 
-layout(location = 0) out vec3 fragColor;
+layout(location = 0) out vec3 frag_color;
 
 void main() {
     gl_Position = ubo.view_proj * vec4(position, 1.0);
 
-    fragColor = color;
+    frag_color = color;
 }
+
