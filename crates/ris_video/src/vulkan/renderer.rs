e07d831ae7ad55d1086e6ad8e4f58a32595e1b48
Rismosch Desktop <simon.sutoris@googlemail.com>
2024-03-30 18:25:41 +0100

    implemented swapchain

--- crates/ris_video/src/vulkan/renderer.rs
+++ crates/ris_video/src/vulkan/renderer.rs
@@ -24,6 +24,10 @@ const VALIDATION_ENABLED: bool = false;
 #[cfg(debug_assertions)]
 const VALIDATION_ENABLED: bool = true;
 
+const PREFERRED_FORMAT: vk::Format = vk::Format::B8G8R8A8_SRGB;
+const PREFERRED_COLOR_SPACE: vk::ColorSpaceKHR = vk::ColorSpaceKHR::SRGB_NONLINEAR;
+const PREFERRED_PRESENT_MODE: vk::PresentModeKHR = vk::PresentModeKHR::IMMEDIATE;
+
 unsafe extern "system" fn debug_callback(
     message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
     message_type: vk::DebugUtilsMessageTypeFlagsEXT,
@@ -55,13 +59,19 @@ unsafe extern "system" fn debug_callback(
 }
 
 struct SuitableDevice {
+    // the lower the suitability, the better suited the device is to render. a dedicated gpu would
+    // have a value of 0
+    pub suitability: usize,
     pub graphics_queue_family: u32,
     pub present_queue_family: u32,
     pub physical_device: vk::PhysicalDevice,
+    pub surface_capabilities: vk::SurfaceCapabilitiesKHR,
+    pub surface_formats: Vec<vk::SurfaceFormatKHR>,
+    pub surface_present_modes: Vec<vk::PresentModeKHR>,
 }
 
 pub struct Renderer {
-    _entry: ash::Entry,
+    entry: ash::Entry,
     instance: ash::Instance,
     debug_utils: Option<(ash::extensions::ext::DebugUtils, vk::DebugUtilsMessengerEXT)>,
     surface_loader: ash::extensions::khr::Surface,
@@ -69,6 +79,12 @@ pub struct Renderer {
     device: ash::Device,
     graphics_queue: vk::Queue,
     present_queue: vk::Queue,
+    swapchain_loader: ash::extensions::khr::Swapchain,
+    swapchain: vk::SwapchainKHR,
+    swapchain_format: vk::SurfaceFormatKHR,
+    swapchain_extent: vk::Extent2D,
+    swapchain_images: Vec<vk::Image>,
+
 //    pub instance: Arc<Instance>,
 //    pub device: Arc<Device>,
 //    pub queue: Arc<Queue>,
@@ -91,8 +107,8 @@ impl Drop for Renderer {
         ris_log::debug!("dropping renderer...");
 
         unsafe {
+            self.swapchain_loader.destroy_swapchain(self.swapchain, None);
             self.device.destroy_device(None);
-
             self.surface_loader.destroy_surface(self.surface, None);
 
             if let Some((debug_utils, debug_utils_messenger)) = self.debug_utils.take() {
@@ -100,7 +116,6 @@ impl Drop for Renderer {
             }
 
             self.instance.destroy_instance(None);
-
         }
 
         ris_log::info!("renderer dropped!");
@@ -245,7 +260,7 @@ impl Renderer {
         let surface: vk::SurfaceKHR = vk::Handle::from_raw(surface_raw);
         let surface_loader = ash::extensions::khr::Surface::new(&entry, &instance);
 
-        // physical device
+        // setup to find suitable physical devices
         let physical_devices = unsafe {
             instance.enumerate_physical_devices()?
         };
@@ -259,28 +274,29 @@ impl Renderer {
         }
         ris_log::debug!("{}", log_message);
 
-        for &physical_device in physical_devices.iter() {
+        // find suitable physical devices
+        for (i, &physical_device) in physical_devices.iter().enumerate() {
 
             // gather physical device information
             let device_properties = unsafe {instance.get_physical_device_properties(physical_device)};
             let device_features = unsafe {instance.get_physical_device_features(physical_device)};
             let device_queue_families = unsafe {instance.get_physical_device_queue_family_properties(physical_device)};
 
-            let device_type = match device_properties.device_type {
-                vk::PhysicalDeviceType::CPU => "cpu",
-                vk::PhysicalDeviceType::INTEGRATED_GPU => "integrated gpu",
-                vk::PhysicalDeviceType::DISCRETE_GPU => "descrete gpu",
-                vk::PhysicalDeviceType::VIRTUAL_GPU => "virtual gpu",
-                vk::PhysicalDeviceType::OTHER => "unkown",
-                _ => panic!(),
+            let (suitability, device_type_name) = match device_properties.device_type {
+                vk::PhysicalDeviceType::DISCRETE_GPU => (0, "discrete gpu"),
+                vk::PhysicalDeviceType::INTEGRATED_GPU => (1, "integrated gpu"),
+                vk::PhysicalDeviceType::VIRTUAL_GPU => (2, "virtual gpu"),
+                vk::PhysicalDeviceType::CPU => (3, "cpu"),
+                vk::PhysicalDeviceType::OTHER => (4, "unkown"),
+                _ => continue,
             };
 
-            let mut log_message = String::from("Vulkan Physical Device");
+            let mut log_message = format!("Vulkan Physical Device {}", i);
 
             let device_name = unsafe {util::VkStr::from(&device_properties.device_name)}?;
             log_message.push_str(&format!("\n\tname: {}", device_name));
             log_message.push_str(&format!("\n\tid: {}", device_properties.device_id));
-            log_message.push_str(&format!("\n\ttype: {}", device_type));
+            log_message.push_str(&format!("\n\ttype: {}", device_type_name));
 
             let api_version_variant = vk::api_version_variant(device_properties.api_version);
             let api_version_major = vk::api_version_major(device_properties.api_version);
@@ -296,7 +312,7 @@ impl Renderer {
             log_message.push_str(&format!("\n\tapi version: {}", api_version));
 
             log_message.push_str(&format!("\n\tsupported queue families: {}", device_queue_families.len()));
-            log_message.push_str("\n\tqueue | graphics, compute, transfer, sparse binding");
+            log_message.push_str("\n\t\tqueue | graphics, compute, transfer, sparse binding");
             for queue_family in device_queue_families.iter() {
                 let supports_graphics = queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS);
                 let supports_compute = queue_family.queue_flags.contains(vk::QueueFlags::COMPUTE);
@@ -304,7 +320,7 @@ impl Renderer {
                 let supports_sparse_binding = queue_family.queue_flags.contains(vk::QueueFlags::SPARSE_BINDING);
 
                 log_message.push_str(&format!(
-                    "\n\t{:5} | {:8}, {:7}, {:8}, {:14}",
+                    "\n\t\t{:5} | {:8}, {:7}, {:8}, {:14}",
                     queue_family.queue_count,
                     supports_graphics,
                     supports_compute,
@@ -348,10 +364,30 @@ impl Renderer {
             //    log_message.push_str(&format!("\n\t\t- {}", name));
             //}
 
+            // check swapchain support
+            let surface_capabilities = unsafe {
+                surface_loader.get_physical_device_surface_capabilities(physical_device, surface)
+            }?;
+            let surface_formats = unsafe {
+                surface_loader.get_physical_device_surface_formats(physical_device, surface)
+            }?;
+            let surface_present_modes = unsafe {
+                surface_loader.get_physical_device_surface_present_modes(physical_device, surface)
+            }?;
+
+            log_message.push_str(&format!("\n\tsurface formats: {}", surface_formats.len()));
+            for format in surface_formats.iter() {
+                log_message.push_str(&format!("\n\t\t- {:?}, {:?}", format.format, format.color_space));
+            }
+            log_message.push_str(&format!("\n\tsurface present modes: {}", surface_present_modes.len()));
+            for present_mode in surface_present_modes.iter() {
+                log_message.push_str(&format!("\n\t\t- {:?}", present_mode));
+            }
+
             ris_log::info!("{}", log_message);
 
-            if !supports_required_extensions {
-                continue;
+            if !supports_required_extensions || surface_formats.is_empty() || surface_present_modes.is_empty() {
+                continue; // device not supported. skip
             }
 
             // find queue family
@@ -394,18 +430,27 @@ impl Renderer {
             }
 
             if let (Some(graphics), Some(present)) = (graphics_queue_index, present_queue_index) {
+                // device is supported and suitable. collect info and add to vec
                 let suitable_device = SuitableDevice{
+                    suitability,
                     graphics_queue_family: graphics as u32,
                     present_queue_family: present as u32,
                     physical_device,
+                    surface_capabilities,
+                    surface_formats,
+                    surface_present_modes,
                 };
                 suitable_devices.push(suitable_device);
-            }
-        }
+            };
+
+        } // end find suitable physical devices
 
         // logical device
-        // TODO get the best device here
-        let suitable_device = suitable_devices.first().unroll()?;
+        let Some(suitable_device) = suitable_devices
+            .into_iter()
+            .min_by_key(|x| x.suitability) else {
+            return ris_error::new_result!("no suitable hardware found to initialize vulkan renderer");
+        };
 
         let mut unique_queue_families = std::collections::HashSet::new();
         unique_queue_families.insert(suitable_device.graphics_queue_family);
@@ -446,8 +491,104 @@ impl Renderer {
         let graphics_queue = unsafe{device.get_device_queue(suitable_device.graphics_queue_family, 0)};
         let present_queue = unsafe{device.get_device_queue(suitable_device.present_queue_family, 0)};
 
+        // swap chain
+        let preferred_surface_format = suitable_device.surface_formats
+            .iter()
+            .find(|x| x.format == PREFERRED_FORMAT && x.color_space == PREFERRED_COLOR_SPACE);
+        let surface_format = match preferred_surface_format {
+            Some(format) => format,
+            // getting the first format if the preferred format does not exist. this should not
+            // cause ub, becuase we checked if the list is empty at finding the suitable device.
+            None => &suitable_device.surface_formats[0],
+        };
+
+        let preferred_surface_present_mode = suitable_device.surface_present_modes
+            .iter()
+            .find(|&&x| x == PREFERRED_PRESENT_MODE);
+        let surface_present_mode = match preferred_surface_present_mode {
+            Some(present_mode) => present_mode,
+            // getting the first present mode if the preferred format does not exist. this should
+            // not cause ub, becuase we checked if the list is empty at finding the suitable device.
+            None => unsafe{suitable_device.surface_present_modes.get_unchecked(0)},
+        };
+
+        let surface_capabilities = suitable_device.surface_capabilities;
+        let swapchain_extent = if surface_capabilities.current_extent.width != u32::MAX {
+            surface_capabilities.current_extent
+        } else {
+            let (window_width, window_height) = window.vulkan_drawable_size();
+            let width = window_width.clamp(
+                surface_capabilities.min_image_extent.width,
+                surface_capabilities.max_image_extent.width,
+            );
+            let height = window_height.clamp(
+                surface_capabilities.min_image_extent.height,
+                surface_capabilities.max_image_extent.height,
+            );
+
+            vk::Extent2D {
+                width,
+                height,
+            }
+        };
+
+        let preferred_swapchain_image_count = surface_capabilities.min_image_count + 1;
+        let swapchain_image_count = if surface_capabilities.max_image_count == 0 {
+            // SurfaceCapabilitiesKHR == 0 indicates there is no maximum
+            preferred_swapchain_image_count
+        } else {
+            u32::min(
+                preferred_swapchain_image_count,
+                surface_capabilities.max_image_count,
+            )
+        };
+
+        let (image_sharing_mode, queue_family_index_count, queue_family_indices) = 
+            if suitable_device.graphics_queue_family != suitable_device.present_queue_family {(
+                vk::SharingMode::CONCURRENT,
+                2,
+                vec![
+                suitable_device.graphics_queue_family,
+                suitable_device.present_queue_family,
+                ],
+            )} else {(
+                vk::SharingMode::EXCLUSIVE,
+                0,
+                vec![],
+            )};
+
+        let swapchain_create_info = vk::SwapchainCreateInfoKHR {
+            s_type: vk::StructureType::SWAPCHAIN_CREATE_INFO_KHR,
+            p_next: ptr::null(),
+            flags: vk::SwapchainCreateFlagsKHR::empty(),
+            surface,
+            min_image_count: swapchain_image_count,
+            image_color_space: surface_format.color_space,
+            image_format: surface_format.format,
+            image_extent: swapchain_extent,
+            image_usage: vk::ImageUsageFlags::COLOR_ATTACHMENT,
+            image_sharing_mode,
+            p_queue_family_indices: queue_family_indices.as_ptr(),
+            queue_family_index_count,
+            pre_transform: surface_capabilities.current_transform,
+            composite_alpha: vk::CompositeAlphaFlagsKHR::OPAQUE,
+            present_mode: *surface_present_mode,
+            clipped: vk::TRUE,
+            old_swapchain: vk::SwapchainKHR::null(),
+            image_array_layers: 1,
+        };
+
+        let swapchain_loader = ash::extensions::khr::Swapchain::new(&instance, &device);
+        let swapchain = unsafe {
+            swapchain_loader.create_swapchain(&swapchain_create_info, None)
+        }?;
+
+        let swapchain_images = unsafe {
+            swapchain_loader.get_swapchain_images(swapchain)
+        }?;
+
         Ok(Self {
-            _entry: entry,
+            entry,
             instance,
             debug_utils,
             surface_loader,
@@ -455,6 +596,11 @@ impl Renderer {
             device,
             graphics_queue,
             present_queue,
+            swapchain_loader,
+            swapchain,
+            swapchain_format: *surface_format,
+            swapchain_extent,
+            swapchain_images,
         })
 
         //// instance
