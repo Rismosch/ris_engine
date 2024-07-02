835bb7c53a4a8910075358fe6c7e3f283f28fc2b
Rismosch Desktop <simon.sutoris@googlemail.com>
2024-07-02 23:19:12 +0200

    further work on profiler html renderer

--- cli/src/commands/profiler_html.rs
+++ cli/src/commands/profiler_html.rs
@@ -8,6 +8,33 @@ use ris_file::io::FatPtr;
 use crate::ExplanationLevel;
 use crate::ICommand;
 
+
+const ORG_NAME: &str = "Rismosch";
+const APP_NAME: &str = "ris_engine";
+const PROFILER: &str = "profiler";
+
+const SEPARATOR: char = ';';
+
+const KEYS: &[&str] = &[
+    "parent",
+    "id",
+    "generation",
+    "file",
+    "line",
+    "min",
+    "max",
+    "sum",
+    "average",
+    "median",
+    "percentage",
+];
+
+#[derive(Debug)]
+struct ParsedCsvLine {
+    parent: String,
+    json: String,
+}
+
 pub struct ProfilerHtml;
 
 impl ICommand for ProfilerHtml {
@@ -20,19 +47,108 @@ impl ICommand for ProfilerHtml {
     }
 
     fn run(_args: Vec<String>, target_dir: PathBuf) -> RisResult<()> {
-        let pref_path = sdl2::filesystem::pref_path("Rismosch", "ris_engine")?;
-        let profiler_dir = PathBuf::from(pref_path).join("profiler");
-
         let chart_js_path = crate::util::get_root_dir()?
-            .join("cli")
+            .join("external")
             .join("javascript")
             .join("Chart.js");
         eprintln!("reading... {:?}", chart_js_path);
         let chart_js = read_text_file(chart_js_path)?;
 
+
+        let pref_path = sdl2::filesystem::pref_path(ORG_NAME, APP_NAME)?;
+        let profiler_dir = PathBuf::from(pref_path).join(PROFILER);
+        eprintln!("reading... {:?}", profiler_dir);
+
+        let mut parsed_csv_files = Vec::new();
+
+        for (i, entry) in profiler_dir.read_dir()?.enumerate() {
+            let entry = entry?;
+            let path = entry.path();
+            let metadata = entry.metadata()?;
+
+            let entry_is_file = metadata.is_file();
+            let path_ends_with_csv = path
+                .extension()
+                .map(|x| x.to_ascii_lowercase() == "csv")
+                .unwrap_or(false);
+
+            if !entry_is_file || !path_ends_with_csv {
+                eprintln!("cannot read {:?}", path);
+                continue;
+            }
+
+            eprintln!("reading... {:?}", path);
+            let file_name = match path.file_name().map(|x| x.to_str()) {
+                Some(Some(file_name)) => file_name.to_string(),
+                _ => format!("csv {}", i),
+            };
+
+            eprintln!("parse csv... {:?}", path);
+            let csv = read_text_file(&path)?;
+            let mut lines = csv.lines();
+
+            let Some(first_line) = lines.next() else {
+                eprintln!("csv contains no lines");
+                continue;
+            };
+
+            let headers = first_line
+                .split(SEPARATOR)
+                .collect::<Vec<_>>();
+            if let Err(e) = ris_error::assert!(headers.len() == KEYS.len()) {
+                eprintln!("failed to parse header: {}", e);
+                continue;
+            }
+
+            for (i, &header) in headers.iter().enumerate() {
+                let key = KEYS[i];
+                if header != key {
+                    eprintln!(
+                        "expected header {} to be \"{}\" but was \"{}\"",
+                        i, key, header,
+                    );
+                    continue;
+                }
+            }
+
+            // header is as expected
+            let mut failed_to_parse = false;
+            
+            let mut parsed_csv_lines = Vec::new();
+            for (i, line) in lines.skip(1).enumerate() {
+                let parsed = match parse_csv_line(line) {
+                    Ok(parsed) => parsed,
+                    Err(e) => {
+                        eprintln!("failed to parse line {}: {}", i + 1, e);
+                        failed_to_parse = true;
+                        break;
+                    },
+                };
+
+                let position = parsed_csv_lines
+                    .iter()
+                    .position(|(x, _)| *x == parsed.parent);
+
+                match position {
+                    None => parsed_csv_lines.push((parsed.parent.clone(), vec![parsed])),
+                    Some(i) => parsed_csv_lines[i].1.push(parsed),
+                }
+            }
+
+            if failed_to_parse {
+                continue;
+            }
+
+            parsed_csv_files.push((file_name, parsed_csv_lines));
+            eprintln!("success parsing!");
+        }
+
+        parsed_csv_files.sort_by(|left, right| left.0.cmp(&right.0));
+
         eprintln!("generating html...");
 
-        let html = format!("
+        let mut html = String::new();
+        html.push_str(&format!("
 <!DOCTYPE html>
 <html>
 <script>
@@ -40,9 +156,55 @@ impl ICommand for ProfilerHtml {
 </script>
 <body>
 
+<select id=\"csv_file\" onchange=\"csv_file_changed()\">", chart_js));
+
+        for (file_name, _) in parsed_csv_files.iter() {
+            html.push_str(&format!("
+<option>{}</option>", file_name));
+        }
+
+        html.push_str(&format!("
+</select>
+
 <canvas id=\"myChart\" style=\"width:100%;max-width:600px\"></canvas>
 
 <script>
+
+var csv_data = {{"));
+        
+        for (file_name, parsed_csv_lines) in parsed_csv_files.iter() {
+            html.push_str(&format!("
+    {}: {{", sanitize_key(file_name)));
+
+            for (parent, lines) in parsed_csv_lines.iter() {
+                html.push_str(&format!("
+        {}: [", sanitize_key(parent)));
+
+                 for line in lines.iter() {
+                     html.push_str(&format!("
+             {},", line.json));
+                 }
+
+                html.push_str(&format!("
+        ],"));
+            }
+
+            html.push_str(&format!("
+    }},"));
+        }
+
+        html.push_str(&format!("
+}};
+
+console.log(csv_data);
+
+var csv_file_select = document.getElementById(\"csv_file\");
+csv_file_changed();
+function csv_file_changed() {{
+    var value = csv_file_select.value;
+    console.log(value);
+}}
+
 const xValues = [\"Italy\", \"France\", \"Spain\", \"USA\", \"Argentina\"];
 const yValues = [55, 49, 44, 24, 15];
 const barColors = [\"red\", \"green\",\"blue\",\"orange\",\"brown\"];
@@ -73,11 +235,7 @@ new Chart(\"myChart\", {{
 </script>
 
 </body>
-</html>
-",
-            chart_js
-        );
-
+</html>"));
 
         eprintln!("writing html...");
         ris_file::util::clean_or_create_dir(&target_dir)?;
@@ -102,3 +260,51 @@ fn read_text_file(path: impl AsRef<Path>) -> RisResult<String> {
     Ok(text)
 }
 
+fn parse_csv_line(line: &str) -> RisResult<ParsedCsvLine> {
+    let cells = line
+        .split(SEPARATOR)
+        .collect::<Vec<_>>();
+    ris_error::assert!(cells.len() == KEYS.len())?;
+
+    let mut json = String::from("{ ");
+    json.push_str(&format!("{}: \"{}\"", KEYS[0], cells[0]));
+    json.push_str(", ");
+    json.push_str(&format!("{}: \"{}\"", KEYS[1], cells[1]));
+    json.push_str(", ");
+    json.push_str(&format!("{}: {}", KEYS[2], cells[2]));
+    json.push_str(", ");
+    json.push_str(&format!("{}: \"{}\"", KEYS[3], cells[3]));
+    json.push_str(", ");
+    json.push_str(&format!("{}: {}", KEYS[4], cells[4]));
+    json.push_str(", ");
+    json.push_str(&format!("{}: {}", KEYS[5], cells[5]));
+    json.push_str(", ");
+    json.push_str(&format!("{}: {}", KEYS[6], cells[6]));
+    json.push_str(", ");
+    json.push_str(&format!("{}: {}", KEYS[7], cells[7]));
+    json.push_str(", ");
+    json.push_str(&format!("{}: {}", KEYS[8], cells[8]));
+    json.push_str(", ");
+    json.push_str(&format!("{}: {}", KEYS[9], cells[9]));
+    json.push_str(", ");
+    json.push_str(&format!("{}: {}", KEYS[10], cells[10]));
+    json.push_str(" }");
+
+    Ok(ParsedCsvLine{
+        parent: cells[0].to_string(),
+        json,
+    })
+}
+
+fn sanitize_key(key: &str) -> String {
+    format!(
+        "_{}",
+        key
+            .replace('+', "_")
+            .replace('-', "_")
+            .replace('.', "_")
+            .replace(' ', "_")
+            .replace('(', "_")
+            .replace(')', "_")
+    )
+}
--- external/README.md
+++ external/README.md
@@ -1,5 +1,11 @@
 All files in `./external/` are not mine and are licensed under their respective licenses.
 
+Project: Chart.js  
+URL: https://www.chartjs.org  
+License: MIT  
+Upstream version: 2.9.4  
+Local modifications: None 
+
 Project: SDL2  
 URL: https://www.libsdl.org/  
 License: zlib  
