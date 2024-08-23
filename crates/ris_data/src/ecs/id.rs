e5df5db3dd5a0d71a48cbaf8606f6d0ce12d507a
Rismosch Desktop <simon.sutoris@googlemail.com>
2024-08-24 00:08:44 +0200

    added bareboned script component

--- crates/ris_data/src/ecs/id.rs
+++ crates/ris_data/src/ecs/id.rs
@@ -2,6 +2,7 @@ use std::marker::PhantomData;
 
 use super::game_object::GameObject;
 use super::mesh_component::MeshComponent;
+use super::script_component::ScriptComponent;
 
 //
 // ids
@@ -84,3 +85,4 @@ impl<T, Id: Eq> Eq for Handle<T, Id> {}
 
 pub type GameObjectHandle = Handle<GameObject, GameObjectId>;
 pub type MeshComponentHandle = Handle<MeshComponent, IndexId>;
+pub type ScriptComponentHandle = Handle<ScriptComponent, IndexId>;
--- crates/ris_data/src/ecs/script_component.rs
+++ crates/ris_data/src/ecs/script_component.rs
@@ -1,34 +1,32 @@
-//use crate::ptr::ArefCell;
-//use crate::ptr::StrongPtr;
-//use crate::ptr::WeakPtr;
-//
-//use super::id::EcsObject;
-//use super::id::IndexId;
-//use super::id::ScriptHandle;
-//
-//pub struct Script {
-//    // identification
-//    handle: ComponentHandle,
-//    is_alive: bool,
-//}
-//
-//impl IComponent for Script {
-//    fn type_id() -> usize {
-//        super::id::COMPONENT_TYPE_ID_SCRIPT
-//    }
-//
-//    fn new(handle: ComponentHandle, is_alive: bool) -> Self {
-//        Self {
-//            handle,
-//            is_alive,
-//        }
-//    }
-//
-//    fn handle(&self) -> ComponentHandle {
-//        self.handle
-//    }
-//
-//    fn is_alive(&self) -> bool {
-//        self.is_alive
-//    }
-//}
+use crate::ptr::ArefCell;
+use crate::ptr::StrongPtr;
+use crate::ptr::WeakPtr;
+
+use super::id::EcsObject;
+use super::id::IndexId;
+use super::id::ScriptComponentHandle;
+
+pub struct ScriptComponent {
+    // identification
+    handle: ScriptComponentHandle,
+    is_alive: bool,
+}
+
+impl ScriptComponent {
+    pub fn new(handle: ScriptComponentHandle, is_alive: bool) -> Self {
+        Self {
+            handle,
+            is_alive,
+        }
+    }
+}
+
+impl EcsObject<IndexId> for ScriptComponent {
+    fn handle(&self) -> ScriptComponentHandle {
+        self.handle
+    }
+
+    fn is_alive(&self) -> bool {
+        self.is_alive
+    }
+}
