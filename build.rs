f9e1f0f1edc45cea5ed11ac01c1399c7f55be8f0
Rismosch Desktop <simon.sutoris@googlemail.com>
2025-02-15 16:19:59 +0100

    cleaning up

--- build.rs
+++ build.rs
@@ -5,50 +5,43 @@ fn main() {
 }
 
 fn build_imgui() {
-    // generate bindings
-    let include_dir = "third_party/imgui";
-    let target_dir = "bindings_imgui";
+    let imgui_dir = "third_party/imgui";
 
+    // generate bindings
+    let target_name = "bindings_imgui";
+
+    generate_bindings(format!("{}/imconfig.h", imgui_dir), imgui_dir, target_name);
+    generate_bindings(format!("{}/imgui.h", imgui_dir), imgui_dir, target_name);
     generate_bindings(
-        "third_party/imgui/imconfig.h",
-        include_dir,
-        target_dir,
+        format!("{}/imgui_internal.h", imgui_dir),
+        imgui_dir,
+        target_name,
     );
     generate_bindings(
-        "third_party/imgui/imgui.h",
-        include_dir,
-        target_dir,
+        format!("{}/imstb_rectpack.h", imgui_dir),
+        imgui_dir,
+        target_name,
     );
     generate_bindings(
-        "third_party/imgui/imgui_internal.h",
-        include_dir,
-        target_dir,
+        format!("{}/imstb_textedit.h", imgui_dir),
+        imgui_dir,
+        target_name,
     );
     generate_bindings(
-        "third_party/imgui/imstb_rectpack.h",
-        include_dir,
-        target_dir,
-    );
-    generate_bindings(
-        "third_party/imgui/imstb_textedit.h",
-        include_dir,
-        target_dir,
-    );
-    generate_bindings(
-        "third_party/imgui/imstb_truetype.h",
-        include_dir,
-        target_dir,
+        format!("{}/imstb_truetype.h", imgui_dir),
+        imgui_dir,
+        target_name,
     );
 
     // compile and link
     cc::Build::new()
         .cpp(true)
-        .include(include_dir)
-        .file("third_party/imgui/imgui.cpp")
-        .file("third_party/imgui/imgui_demo.cpp")
-        .file("third_party/imgui/imgui_draw.cpp")
-        .file("third_party/imgui/imgui_tables.cpp")
-        .file("third_party/imgui/imgui_widgets.cpp")
+        .include(imgui_dir)
+        .file(format!("{}/imgui.cpp", imgui_dir))
+        .file(format!("{}/imgui_demo.cpp", imgui_dir))
+        .file(format!("{}/imgui_draw.cpp", imgui_dir))
+        .file(format!("{}/imgui_tables.cpp", imgui_dir))
+        .file(format!("{}/imgui_widgets.cpp", imgui_dir))
         .flag_if_supported("-std=c++17")
         .compile("imgui");
 
@@ -59,7 +52,15 @@ fn build_imgui() {
     //}
 }
 
-fn generate_bindings(header: &str, include_dir: &str, target_dir: &str) {
+fn generate_bindings(
+    header: impl AsRef<str>,
+    include_dir: impl AsRef<str>,
+    target_name: impl AsRef<str>,
+) {
+    let header = header.as_ref();
+    let include_dir = include_dir.as_ref();
+    let target_name = target_name.as_ref();
+
     println!("cargo:rerun-if-changed={}", header);
 
     let bindings = bindgen::Builder::default()
@@ -76,16 +77,17 @@ fn generate_bindings(header: &str, include_dir: &str, target_dir: &str) {
         .file_stem()
         .expect("header path had no file name")
         .to_str()
-        .expect("failed to convert OsStr to str")
-        .replace('.', "_");
+        .expect("failed to convert OsStr to str");
 
     let out_dir = std::env::var("OUT_DIR").expect("failed to resolve env OUT_DIR");
     let target_path = PathBuf::from(out_dir)
-        .join(target_dir)
+        .join(target_name)
         .join(format!("{}.rs", filename));
 
     let target_parent = target_path.parent().expect("target_path had no parent");
     std::fs::create_dir_all(target_parent).expect("failed to create target_parent");
 
-    bindings.write_to_file(&target_path).expect("failed to write bindings");
+    bindings
+        .write_to_file(&target_path)
+        .expect("failed to write bindings");
 }
