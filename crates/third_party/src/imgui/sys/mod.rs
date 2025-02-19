e99f4910f1bb216d0ba65b13815609cead3a46b8
Rismosch Desktop <simon.sutoris@googlemail.com>
2025-02-19 23:32:51 +0100

    moved bindings

--- crates/third_party/src/imgui/sys/mod.rs
+++ crates/third_party/src/imgui/sys/mod.rs
@@ -1,8 +1,11 @@
-pub mod backends;
-
-pub mod imconfig;
-pub mod imgui;
-pub mod imgui_internal;
-pub mod imstb_rectpack;
-pub mod imstb_textedit;
-pub mod imstb_truetype;
+cfg_if::cfg_if! {
+    if #[cfg(target_os = "windows")] {
+        mod windows;
+        pub use windows::*;
+    } else if #[cfg(target_os = "linux")] {
+        mod linux;
+        pub use linux::*;
+    } else {
+        compile_error!("imgui bindings haven't been build this os yet");
+    }
+}
