2c1ee60fc51b08d8de6c78df5c7c808279e3e9d0
Rismosch Desktop <simon.sutoris@googlemail.com>
2025-02-24 20:44:47 +0100

    can now properly wake workers

--- crates/ris_async/src/thread_pool.rs
+++ crates/ris_async/src/thread_pool.rs
@@ -1,11 +1,14 @@
 use std::cell::RefCell;
 use std::cell::UnsafeCell;
 use std::marker::PhantomData;
+use std::pin::Pin;
 use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering;
 use std::future::Future;
+use std::task::Wake;
 use std::thread::JoinHandle;
+use std::thread::Thread;
 
 use ris_error::Extensions;
 use ris_error::RisResult;
@@ -19,6 +22,14 @@ use crate::SpinLock;
 pub const DEFAULT_BUFFER_CAPACITY: usize = 1024;
 
 type Job = Box<dyn Future<Output = ()>>;
+
+struct ThreadWaker(Thread);
+
+impl Wake for ThreadWaker {
+    fn wake(self: Arc<Self>) {
+        self.0.unpark();
+    }
+}
 
 thread_local! {
     static WORKER: UnsafeCell<Option<Worker>> = const { UnsafeCell::new(None)};
@@ -40,7 +51,12 @@ pub struct Worker {
     done: Arc<AtomicBool>,
     sender: Sender<Job>,
     receiver: Receiver<Job>,
-    stealers: Vec<Arc<Stealer<Job>>>,
+    shared: Vec<SharedWorkerData>,
+}
+
+pub struct SharedWorkerData {
+    stealer: Arc<Stealer<Job>>,
+    waker: Arc<ThreadWaker>,
 }
 
 impl Worker {
@@ -52,28 +68,51 @@ impl Worker {
 
     fn run(&self) {
         println!("running \"{}\"", self.name());
+
         while !self.done.load(Ordering::Relaxed) {
             if !self.run_pending_job() {
                 std::thread::park();
             }
         }
+
         println!("finishing... \"{}\"", self.name());
+        while self.run_pending_job() {}
+        println!("finished \"{}\"!", self.name());
     }
 
     fn run_pending_job(&self) -> bool {
-        match self.receiver.receive() {
-            Some(job) => true,
-            None => {
-                for stealer in self.stealers.iter() {
-                    let Some(job) = stealer.steal() else {
-                        continue;
-                    };
+        match self.get_job() {
+            Some(job) => {
+                let pinned = Pin::from(job);
+                self.block_on(pinned);
+                true
+            },
+            None => false,
+        }
+    }
 
-                    return true;
+    fn get_job(&self) -> Option<Job> {
+        match self.receiver.receive() {
+            Some(job) => Some(job),
+            None => {
+                for SharedWorkerData { stealer, waker } in self.shared.iter() {
+                    if let Some(job) = stealer.steal() {
+                        return Some(job)
+                    };
                 }
 
-                false
+                None
             }
+        }
+    }
+
+    fn block_on<F: Future>(&self, future: F) -> F::Output {
+        panic!();
+    }
+
+    fn wake_other_workers(&self) {
+        for SharedWorkerData { stealer, waker } in self.shared.iter() {
+            waker.clone().wake();
         }
     }
 }
@@ -91,9 +130,8 @@ impl Drop for ThreadPool {
 
         match get_worker() {
             Some(worker) => {
-                while let Some(job) = worker.receiver.receive() {
-
-                }
+                worker.wake_other_workers();
+                while worker.run_pending_job() {}
             },
             None => {
                 println!("thread pool was dropped on a non worker thread")
@@ -106,7 +144,7 @@ impl Drop for ThreadPool {
                     let i = i + 1;
                     match join_handle.join() {
                         Ok(()) => println!("joined thread {}", i),
-                        Err(e) => println!("failed to join thread {}", i),
+                        Err(_) => println!("failed to join thread {}", i),
                     }
                 }
             },
@@ -137,24 +175,25 @@ impl ThreadPool {
         }
 
         // setup stealers
-        let stealer_originals = Arc::new(SpinLock::new(Vec::new()));
-        let stealer_copies = Arc::new(SpinLock::new(Vec::<Option<_>>::new()));
-        let copied_stealers = Arc::new(AtomicBool::new(false));
+        let original_worker_data = Arc::new(SpinLock::new(Vec::new()));
+        let shared_worker_data = Arc::new(SpinLock::new(Vec::<Option<_>>::new()));
+        let done_preparing_shared_worker_data = Arc::new(AtomicBool::new(false));
 
-        let mut g = stealer_originals.lock();
+        let mut g = original_worker_data.lock();
         for _ in 0..threads {
             g.push(None);
         }
         drop(g);
 
-        // setup main thread
+        // initial main worker setup
         crate::affinity::set_affinity(&affinities[0]);
         let (
             sender,
             receiver,
             stealer,
         ) = Channel::<Job>::new(buffer_capacity);
-        stealer_originals.lock()[0] = Some(stealer);
+        let waker = Arc::new(ThreadWaker(std::thread::current()));
+        original_worker_data.lock()[0] = Some((stealer, waker));
 
         // setup worker threads
         let done = Arc::new(AtomicBool::new(false));
@@ -163,47 +202,48 @@ impl ThreadPool {
         for (i, core_ids) in affinities.iter().enumerate().take(threads).skip(1) {
             let core_ids = core_ids.clone();
             let done = done.clone();
-            let stealer_originals = stealer_originals.clone();
-            let stealer_copies = stealer_copies.clone();
-            let copied_stealers = copied_stealers.clone();
+            let original_worker_data = original_worker_data.clone();
+            let shared_worker_data = shared_worker_data.clone();
+            let done_preparing_shared_worker_data = done_preparing_shared_worker_data.clone();
 
             let join_handle = std::thread::Builder::new()
                 .name(format!("thread pool worker {}", i))
                 .spawn(move || {
-                    // setup worker
+                    // worker initial setup
                     crate::affinity::set_affinity(&core_ids);
                     let (
                         sender,
                         receiver,
                         stealer,
                     ) = Channel::<Job>::new(buffer_capacity);
+                    let waker = Arc::new(ThreadWaker(std::thread::current()));
+                    original_worker_data.lock()[i] = Some((stealer, waker));
 
-                    stealer_originals.lock()[i] = Some(stealer);
-                    //stealer_originals.lock()[i] = Some(i);
-
-                    while !copied_stealers.load(Ordering::Relaxed) {
+                    while !done_preparing_shared_worker_data.load(Ordering::Relaxed) {
                         std::thread::yield_now();
                     }
 
-                    let mut g = stealer_copies.lock();
-                    let stealers = ris_error::unwrap!(
+                    // prepare worker
+                    let mut g = shared_worker_data.lock();
+                    let shared = ris_error::unwrap!(
                         (&mut g[i]).take().into_ris_error(),
                         "something has gone terribly wrong. this option should never be none"
                     );
                     drop(g);
 
-                    // prepare worker and run
                     set_worker(Worker{
                         done,
                         sender,
                         receiver,
-                        stealers,
+                        shared,
                     });
 
                     let worker = ris_error::unwrap!(
                         get_worker().into_ris_error(),
                         "something has gone terribly wrong. this option should never be none"
                     );
+
+                    // run worker
                     worker.run();
                 })?;
             join_handles.push(join_handle);
@@ -211,15 +251,15 @@ impl ThreadPool {
 
         // wait until all workers have set up their channels
         for i in 0..threads {
-            while stealer_originals.lock()[i].is_none() {
+            while original_worker_data.lock()[i].is_none() {
                 std::thread::yield_now();
             }
         }
 
         // all channels are setup, stealers can now be copied
         for i in 0..threads {
-            let mut copy = Vec::new();
-            let g = stealer_originals.lock();
+            let mut shared = Vec::new();
+            let g = original_worker_data.lock();
             for j in 0..g.len() {
                 // offset j, such that every worker has a different stealer at first, this attempts
                 // to reduce contention
@@ -229,49 +269,62 @@ impl ThreadPool {
                 }
 
                 let original = &g[j];
-                let original_ref = ris_error::unwrap!(
+                let (stealer, waker) = ris_error::unwrap!(
                     original.as_ref().into_ris_error(),
                     "something has gone terribly wrong. this option should never be none"
                 );
 
-                copy.push(original_ref.clone());
+                
+                shared.push(SharedWorkerData{
+                    stealer: stealer.clone(),
+                    waker: waker.clone(),
+                });
             }
             drop(g);
-            stealer_copies.lock().push(Some(copy));
+            shared_worker_data.lock().push(Some(shared));
         }
 
-        copied_stealers.store(true, Ordering::Relaxed);
+        done_preparing_shared_worker_data.store(true, Ordering::Relaxed);
 
-        let mut g = stealer_copies.lock();
-        let stealers = ris_error::unwrap!(
+        // prepare main worker
+        let mut g = shared_worker_data.lock();
+        let shared = ris_error::unwrap!(
             (&mut g[0]).take().into_ris_error(),
             "something has gone terribly wrong. this option should never be none"
         );
         drop(g);
 
-        // prepare main worker and thread pool
         let worker = Worker {
             done: done.clone(),
             sender,
             receiver,
-            stealers,
+            shared,
         };
         set_worker(worker);
 
+        // return thread pool
         Ok(Self {
             done,
             join_handles: Some(join_handles),
         })
     }
 
-    pub fn submit<F: Future>(&self, future: F) {
+    pub fn submit<F: Future>(&self, future: F) -> Result<(), F> {
+        let Some(worker) = get_worker() else {
+            println!("not a worker");
+            return Err(future); // todo better error
+        };
+
         let job = async {
             let output = future.await;
         };
 
         // todo: enqueue `job` into thread pool
         // todo: find a way to return `output`
-        // todo: unpark all threads
+
+        worker.wake_other_workers();
+
+        Ok(())
     }
 
     pub fn run_pending_job() -> bool {
