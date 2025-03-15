a3289382b809abadb7058e597bb918992634a638
Rismosch Desktop <simon.sutoris@googlemail.com>
2025-03-15 19:09:10 +0100

    can now deserialize the json examples of the rfc

--- crates/ris_asset/src/codecs/json.rs
+++ crates/ris_asset/src/codecs/json.rs
@@ -7,10 +7,10 @@ pub const END_ARRAY: char = ']';
 pub const END_OBJECT: char = '}';
 pub const NAME_SEPARATOR: char = ':';
 pub const VALUE_SEPARATOR: char = ',';
-pub const WS_SPACE: char = '\u{20}';
+pub const WS_CARRIAGE_RETURN: char = '\u{0D}';
 pub const WS_HORIZONTAL_TAB: char = '\u{09}';
 pub const WS_LINE_FEED: char = '\u{0A}';
-pub const WS_CARRIAGE_RETURN: char = '\u{0D}';
+pub const WS_SPACE: char = '\u{20}';
 pub const TRUE: &str = "true";
 pub const NULL: &str = "null";
 pub const FALSE: &str = "false";
@@ -21,6 +21,7 @@ pub const PLUS: char = '+';
 pub const ZERO: char = '0';
 pub const ESCAPE: char = '\\';
 pub const QUOTATION_MARK: char = '"';
+pub const BYTE_ORDER_MARK: char = '\u{feff}';
 
 // structs
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -46,7 +47,9 @@ pub struct JsonMember {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct JsonNumber {
-    inner: String, // storing it as any kind of number is a major footgun
+    inner: String, // storing a json number as any kind or combination of ints or floats leads to
+                   // many footguns. thus we store it as a string and convert only when the user
+                   // attempts to read the number
 }
 
 impl Default for JsonValue {
@@ -57,19 +60,21 @@ impl Default for JsonValue {
 
 impl Default for JsonNumber {
     fn default() -> Self {
-        Self{inner: "0".to_string()}
+        Self{inner: ZERO.to_string()}
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum JsonError {
     InvalidCast,
+    SyntaxError,
 }
 
 impl std::fmt::Display for JsonError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::InvalidCast => write!(f, "specified cast is not valid"),
+            Self::SyntaxError => write!(f, "non valid json syntax"),
         }
     }
 }
@@ -325,18 +330,232 @@ impl JsonValue {
         }
     }
 
-    pub fn deserialize(value: impl AsRef<str>) -> Self {
+    pub fn deserialize(value: impl AsRef<str>) -> Result<Self, JsonError> {
         let value = value.as_ref();
 
-        let mut line = 1;
-        let mut column = 1;
+        let mut is_string = false;
+        let mut tokens = vec![String::new()];
 
-        // ignore potential U+FEFF
-        // parse escaped character, including optional escape characters
-        panic!()
+        // ignore byte order mark (U+FEFF) for interopability. see https://www.rfc-editor.org/rfc/rfc8259#section-8.1
+        let skip = if value.chars().next() == Some(BYTE_ORDER_MARK) {
+            1
+        } else {
+            0
+        };
+
+        for c in value.chars().skip(skip) {
+            let last_index = tokens.len() - 1;
+            let token = &mut tokens[last_index];
+
+            if c == QUOTATION_MARK {
+                token.push(c);
+                is_string = !is_string;
+
+                if !is_string {
+                    tokens.push(String::new());
+                }
+
+                continue;
+            }
+
+            if is_string {
+                token.push(c);
+                continue;
+            }
+
+            match c {
+                WS_CARRIAGE_RETURN | WS_SPACE | WS_HORIZONTAL_TAB => {
+                    if token.is_empty() {
+                        continue;
+                    }
+
+                    tokens.push(String::new());
+                }
+                WS_LINE_FEED => {
+
+                    if token.is_empty() {
+                        continue;
+                    }
+
+                    tokens.push(String::new());
+                },
+                BEGIN_ARRAY | BEGIN_OBJECT | END_ARRAY | END_OBJECT | NAME_SEPARATOR | VALUE_SEPARATOR => {
+                    if !token.is_empty() {
+                        tokens.push(String::new());
+                    }
+
+                    let last_index = tokens.len() - 1;
+                    let token = &mut tokens[last_index];
+                    token.push(c);
+                    tokens.push(String::new());
+                },
+            _ => token.push(c),
+            }
+        }
+
+        let last_index = tokens.len() - 1;
+        let last = &tokens[last_index];
+        if last.is_empty() {
+            tokens.remove(last_index);
+        }
+
+        Self::from_tokens(&tokens)
+    }
+
+    fn from_tokens(tokens: &[String]) -> Result<Self, JsonError> {
+        let len = tokens.len();
+        if len == 0 {
+            return Err(JsonError::SyntaxError);
+        }
+
+        if len == 1 {
+            let token = &tokens[0];
+            return Self::from_token(token);
+        }
+
+        // multiple tokens, separate them
+        let first_token = &tokens[0];
+        let last_token = &tokens[len - 1];
+
+        if first_token.len() > 1 {
+            return Err(JsonError::SyntaxError);
+        }
+
+        if last_token.len() > 1 {
+            return Err(JsonError::SyntaxError);
+        }
+
+        let first = first_token.chars().next().unwrap();
+        let last = last_token.chars().next().unwrap();
+
+        let is_object = first == BEGIN_OBJECT && last == END_OBJECT;
+        let is_array = first == BEGIN_ARRAY && last == END_ARRAY;
+
+        if !is_object && !is_array {
+            return Err(JsonError::SyntaxError);
+        }
+
+        let mut object_generation = 0;
+        let mut array_generation = 0;
+
+        let mut elements = vec![Vec::new()];
+
+        for token in &tokens[1..tokens.len() - 1] {
+            match token.len() {
+                0 => return Err(JsonError::SyntaxError),
+                1 => match token.chars().next().unwrap() {
+                    BEGIN_OBJECT => object_generation += 1,
+                    BEGIN_ARRAY => array_generation += 1,
+                    END_OBJECT => object_generation -= 1,
+                    END_ARRAY => array_generation -= 1,
+                    VALUE_SEPARATOR => {
+                        if object_generation == 0 && array_generation == 0 {
+                            elements.push(Vec::new());
+                            continue;
+                        }
+                    },
+                    _ => {},
+                },
+                _ => {},
+            }
+
+            if object_generation < 0 || array_generation < 0 {
+                return Err(JsonError::SyntaxError);
+            }
+
+            let last_index = elements.len() - 1;
+            let element = &mut elements[last_index];
+            element.push(token.clone());
+        }
+
+        let result = if is_object {
+            let members = elements.into_iter()
+                .map(|x| JsonMember::from_tokens(&x))
+                .collect::<Result<Vec<_>, JsonError>>()?;
+            JsonValue::Object(Box::new(JsonObject{members}))
+        } else { // is_array 
+            let values = elements.into_iter()
+                .map(|x| Self::from_tokens(&x))
+                .collect::<Result<Vec<_>, JsonError>>()?;
+            JsonValue::Array(values)
+        };
+
+        Ok(result)
+    }
+
+    fn from_token(token: &str) -> Result<Self, JsonError> {
+        // try parse literal
+        match token {
+            FALSE => return Ok(JsonValue::Boolean(false)),
+            NULL => return Ok(JsonValue::Null),
+            TRUE => return Ok(JsonValue::Boolean(true)),
+            _ => (),
+        }
+
+        // try parse string
+        if token.starts_with(QUOTATION_MARK) && token.ends_with(QUOTATION_MARK) {
+            let inner: String = token.chars()
+                .skip(1)
+                .take(token.chars().count() - 2)
+                .collect();
+
+            // unescape characters
+            let mut result = String::new();
+            let mut char_iter = inner.chars();
+            while let Some(c) = char_iter.next() {
+                if c != ESCAPE {
+                    result.push(c);
+                    continue;
+                }
+
+                let Some(c) = char_iter.next() else {
+                    return Err(JsonError::SyntaxError);
+                };
+
+                match c {
+                    '"' => result.push('"'),
+                    '\\' => result.push('\\'),
+                    '/' => result.push('/'),
+                    'b' => result.push('\u{0008}'),
+                    'f' => result.push('\u{000C}'),
+                    'n' => result.push('\u{000A}'),
+                    'r' => result.push('\u{000D}'),
+                    't' => result.push('\u{0009}'),
+                    'u' => {
+                        let digit_1 = char_iter.next().ok_or(JsonError::SyntaxError)?;
+                        let digit_2 = char_iter.next().ok_or(JsonError::SyntaxError)?;
+                        let digit_3 = char_iter.next().ok_or(JsonError::SyntaxError)?;
+                        let digit_4 = char_iter.next().ok_or(JsonError::SyntaxError)?;
+
+                        let hex_codepoint = format!(
+                            "{}{}{}{}",
+                            digit_1,
+                            digit_2,
+                            digit_3,
+                            digit_4,
+                        );
+                        let codepoint = u32::from_str_radix(&hex_codepoint, 16)
+                            .map_err(|_| JsonError::SyntaxError)?;
+                        let character = char::from_u32(codepoint).ok_or(JsonError::SyntaxError)?;
+
+                        result.push(character);
+                    },
+                    _ => return Err(JsonError::SyntaxError),
+                }
+            }
+
+            return Ok(JsonValue::String(result));
+        }
+
+        // try parse number
+        if token.parse::<f32>().is_ok() {
+            return Ok(JsonValue::Number(JsonNumber{inner: token.to_string()}));
+        }
+
+        // invalid token
+        Err(JsonError::SyntaxError)
     }
 }
-
 
 impl JsonObject {
     pub fn serialize(&self) -> String {
@@ -368,6 +587,37 @@ impl JsonMember {
             NAME_SEPARATOR,
             serialized_value,
         )
+    }
+
+    fn from_tokens(tokens: &[String]) -> Result<Self, JsonError> {
+        if tokens.len() < 3 {
+            return Err(JsonError::SyntaxError);
+        }
+
+        let name_token = &tokens[0];
+        let separator_token = &tokens[1];
+        let value_tokens = &tokens[2..];
+
+        let name_value = JsonValue::from_tokens(&[name_token.clone()])?;
+        let JsonValue::String(name) = name_value else {
+            return Err(JsonError::SyntaxError);
+        };
+
+        if separator_token.len() != 1 {
+            return Err(JsonError::SyntaxError);
+        }
+
+        let separator = separator_token.chars().next().unwrap();
+        if separator != NAME_SEPARATOR {
+            return Err(JsonError::SyntaxError);
+        }
+
+        let value = JsonValue::from_tokens(&value_tokens)?;
+
+        Ok(Self {
+            name,
+            value,
+        })
     }
 }
 
--- tests/suite/ris_asset/json.rs
+++ tests/suite/ris_asset/json.rs
@@ -1,11 +1,73 @@
-use ris_asset::codecs::json::JsonError;
 use ris_asset::codecs::json::JsonObject;
-use ris_asset::codecs::json::JsonMember;
 use ris_asset::codecs::json::JsonValue;
-use ris_asset::codecs::json::JsonNumber;
 
-#[test]
-fn should_serialize_example_1() {
+// examples directly taken from the rfc
+fn example_1_json() -> &'static str {
+    "{
+        \"Image\": {
+            \"Width\":  800,
+            \"Height\": 600,
+            \"Title\":  \"View from 15th Floor\",
+            \"Thumbnail\": {
+                \"Url\":    \"http://www.example.com/image/481989943\",
+                \"Height\": 125,
+                \"Width\":  100
+            },
+            \"Animated\" : false,
+            \"IDs\": [116, 943, 234, 38793]
+        }
+    }"
+}
+
+
+fn example_2_json() -> &'static str {
+
+    // because of floating point imprecision and how numbers are stored and serilialized in rust, the two lines
+    //
+    //     \"Latitude\":  37.371991,
+    //     \"Longitude\": -122.026020,
+    //
+    // have been changed to
+    //
+    //     \"Latitude\":  37.37199,
+    //     \"Longitude\": -122.02602,
+    "[
+        {
+           \"precision\": \"zip\",
+           \"Latitude\":  37.7668,
+           \"Longitude\": -122.3959,
+           \"Address\":   \"\",
+           \"City\":      \"SAN FRANCISCO\",
+           \"State\":     \"CA\",
+           \"Zip\":       \"94107\",
+           \"Country\":   \"US\"
+        },
+        {
+           \"precision\": \"zip\",
+           \"Latitude\":  37.37199,
+           \"Longitude\": -122.02602,
+           \"Address\":   \"\",
+           \"City\":      \"SUNNYVALE\",
+           \"State\":     \"CA\",
+           \"Zip\":       \"94085\",
+           \"Country\":   \"US\"
+        }
+    ]"
+}
+
+fn example_3_json() -> &'static str {
+    "\"Hello world!\""
+}
+
+fn example_4_json() -> &'static str {
+    "42"
+}
+
+fn example_5_json() -> &'static str {
+    "true"
+}
+
+fn example_1_value() -> JsonValue {
     let mut object = JsonObject::default();
     let mut image = JsonObject::default();
     image.push("Width", 800);
@@ -20,13 +82,10 @@ fn should_serialize_example_1() {
     image.push("IDs", &[116, 943, 234, 38793]);
     object.push("Image", image);
 
-    let json = object.serialize();
-    let expected = "{\"Image\":{\"Width\":800,\"Height\":600,\"Title\":\"View from 15th Floor\",\"Thumbnail\":{\"Url\":\"http://www.example.com/image/481989943\",\"Height\":125,\"Width\":100},\"Animated\":false,\"IDs\":[116,943,234,38793]}}";
-    assert_eq!(json, expected);
+    JsonValue::from(object)
 }
 
-#[test]
-fn should_serialize_example_2() {
+fn example_2_value() -> JsonValue {
     let mut object1 = JsonObject::default();
     object1.push("precision", "zip");
     object1.push("Latitude", 37.7668);
@@ -38,108 +97,103 @@ fn should_serialize_example_2() {
     object1.push("Country", "US");
     let mut object2 = JsonObject::default();
     object2.push("precision", "zip");
-    object2.push("Latitude", 37.371991); // because of floating point imprecision this will be
-                                         // stored as 37.37199
-    object2.push("Longitude", -122.026020); // will be stored as -122.02602
+    object2.push("Latitude", 37.37199);
+    object2.push("Longitude", -122.02602);
     object2.push("Address", "");
     object2.push("City", "SUNNYVALE");
     object2.push("State", "CA");
     object2.push("Zip", "94085");
     object2.push("Country", "US");
-    let array = JsonValue::from(&[object1, object2]);
+    JsonValue::from(&[object1, object2])
+}
 
-    println!("what: {:#?}", array);
+fn example_3_value() -> JsonValue {
+    JsonValue::from("Hello world!")
+}
 
-    let json = array.serialize();
+fn example_4_value() -> JsonValue {
+    JsonValue::from(42)
+}
+
+fn example_5_value() -> JsonValue {
+    JsonValue::from(true)
+}
+
+#[test]
+fn should_serialize_example_1() {
+    let example = example_1_value();
+    let json = example.serialize();
+    let expected = "{\"Image\":{\"Width\":800,\"Height\":600,\"Title\":\"View from 15th Floor\",\"Thumbnail\":{\"Url\":\"http://www.example.com/image/481989943\",\"Height\":125,\"Width\":100},\"Animated\":false,\"IDs\":[116,943,234,38793]}}";
+    assert_eq!(json, expected);
+}
+
+#[test]
+fn should_serialize_example_2() {
+    let example = example_2_value();
+    let json = example.serialize();
     let expected = "[{\"precision\":\"zip\",\"Latitude\":37.7668,\"Longitude\":-122.3959,\"Address\":\"\",\"City\":\"SAN FRANCISCO\",\"State\":\"CA\",\"Zip\":\"94107\",\"Country\":\"US\"},{\"precision\":\"zip\",\"Latitude\":37.37199,\"Longitude\":-122.02602,\"Address\":\"\",\"City\":\"SUNNYVALE\",\"State\":\"CA\",\"Zip\":\"94085\",\"Country\":\"US\"}]";
     assert_eq!(json, expected);
 }
 
 #[test]
 fn should_serialize_example_3() {
-    let value = JsonValue::try_from("Hello world!").unwrap();
-    let json = value.serialize();
+    let example = example_3_value();
+    let json = example.serialize();
     assert_eq!(json, "\"Hello world!\"");
 }
 
 #[test]
 fn should_serialize_example_4() {
-    let value = JsonValue::from(42);
-    let json = value.serialize();
+    let example = example_4_value();
+    let json = example.serialize();
     assert_eq!(json, "42");
 }
 
 #[test]
 fn should_serialize_example_5() {
-    let value = JsonValue::from(true);
-    let json = value.serialize();
+    let example = example_5_value();
+    let json = example.serialize();
     assert_eq!(json, "true");
 }
 
 #[test]
 fn should_deserialize_example_1() {
-    let example = "
-        {
-        \"Image\": {
-            \"Width\":  800,
-            \"Height\": 600,
-            \"Title\":  \"View from 15th Floor\",
-            \"Thumbnail\": {
-                \"Url\":    \"http://www.example.com/image/481989943\",
-                \"Height\": 125,
-                \"Width\":  100
-            },
-            \"Animated\" : false,
-            \"IDs\": [116, 943, 234, 38793]
-          }
-      }";
-    panic!();
+    let example = example_1_json();
+    let value = JsonValue::deserialize(example).unwrap();
+    let expected = example_1_value();
+    assert_eq!(value, expected);
 }
 
 #[test]
 fn should_deserialize_example_2() {
-    let example = "
-    [
-        {
-           \"precision\": \"zip\",
-           \"Latitude\":  37.7668,
-           \"Longitude\": -122.3959,
-           \"Address\":   \"\",
-           \"City\":      \"SAN FRANCISCO\",
-           \"State\":     \"CA\",
-           \"Zip\":       \"94107\",
-           \"Country\":   \"US\"
-        },
-        {
-           \"precision\": \"zip\",
-           \"Latitude\":  37.371991,
-           \"Longitude\": -122.026020,
-           \"Address\":   \"\",
-           \"City\":      \"SUNNYVALE\",
-           \"State\":     \"CA\",
-           \"Zip\":       \"94085\",
-           \"Country\":   \"US\"
-        }
-      ]";
-    panic!();
+    let example = example_2_json();
+    let value = JsonValue::deserialize(example).unwrap();
+    let expected = example_2_value();
+    assert_eq!(value, expected);
 }
 
 #[test]
 fn should_deserialize_example_3() {
-    let example = "Hello world!";
-    panic!();
+    let example = example_3_json();
+    let value = JsonValue::deserialize(example).unwrap();
+    let expected = example_3_value();
+    assert_eq!(value, expected);
 }
 
 #[test]
 fn should_deserialize_example_4() {
-    let example = "42";
-    panic!();
+    let example = example_4_json();
+    let value = JsonValue::deserialize(example).unwrap();
+    let expected = example_4_value();
+    assert_eq!(value, expected);
 }
 
 #[test]
 fn should_deserialize_example_5() {
-    let example = "true";
-    panic!();
+    let example = example_5_json();
+    let value = JsonValue::deserialize(example).unwrap();
+    let expected = example_5_value();
+    assert_eq!(value, expected);
 }
 
 #[test]
@@ -158,4 +212,9 @@ fn number_should_not_be_neg_infinity() {
 #[should_panic]
 fn number_should_not_be_nan() {
     let _ = JsonValue::from(f32::NAN);
+}
+
+#[test]
+fn should_deserialize_edge_cases() {
+    panic!("make sure to hit every branch, place a panic in each and generate edgecases until all panics have been removed");
 }
