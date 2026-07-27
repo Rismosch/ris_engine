3b1cde6b77bf3e399a3088a57001b5acba4414e1
Simons Thinkpad <simon.sutoris@googlemail.com>
2026-07-27 14:29:50 +0200

    Cleanup Makefile

--- src/greeter.cpp
+++ src/greeter.cpp
@@ -1,12 +1,10 @@
 #include "greeter.hpp"
 #include <stdio.h>
 
-
 void Greeter::greet() {
 #ifdef NDEBUG
-	printf("i was built without debug!\n");
-    std::cout << "i was built without debug!\n";
+  printf("i was built without debug!\n");
 #else
-	printf("hello :)\n");
+  printf("hello :)\n");
 #endif
 }
